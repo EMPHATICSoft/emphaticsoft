8a850fce5fc6b83ce1d41a1ec736fe1e97394369
Jonathan Paley <jpaley@fnal.gov>
2021-10-05 15:43:23 -0500

    Add basic Geometry code.  Simple geometry for phase 2 is described in gdml file, it probably needs some cleaning up.  Code compiles, but has not been tested.

--- Geometry/Geometry.cxx
+++ Geometry/Geometry.cxx
@@ -1,20 +1,184 @@
+////////////////////////////////////////////////////////////////////////
 /// \file    Geometry.cxx
 /// \brief
-/// \version $Id: Geometry.cxx
+/// \version 
 /// \author  jpaley@fnal.gov
 ////////////////////////////////////////////////////////////////////////
 
+#include <cassert>
+#include <iostream>
+#include <stdio.h>
+
+//ROOT includes
+#include "TGeoManager.h"
+#include "TGeoNode.h"
+#include "TGeoVolume.h"
+#include "TVector3.h"
+#include "TGeoMatrix.h"
+#include "TGeoBBox.h"
+#include "TVirtualGeoPainter.h"
+
+// Framework includes
+#include "messagefacility/MessageLogger/MessageLogger.h"
+#include "cetlib_except/exception.h"
+#include "cetlib/search_path.h"
+
 #include "Geometry.h"
 
-bool geo::Geometry::LoadFromGDML(std::string gdmlFile)
-{
-  _geom = TGeoManager::Import(gdmlFile.c_str());
+namespace geo {
 
-  if (_geom)
+  SSD::SSD() :
+    fName(""), fPos(-1e6,-1e6,-1e6), fDz(-99999), fWidth(0.), fHeight(0.)
+  {
+
+  }
+  
+  //--------------------------------------------------------------------------------
+
+  SSDStation::SSDStation() :
+    fName(""), fPos(-1e6,-1e6,-1e6), fDz(-99999), fWidth(0.), fHeight(0.), fSSD(0)
+  {
+
+  }
+  
+  //--------------------------------------------------------------------------------
+
+  Geometry::Geometry() :
+    fGDMLFile("")
+  {
+    
+  }
+  
+  //--------------------------------------------------------------------------------
+  
+  Geometry::Geometry(std::string fname) :
+    fGDMLFile(fname), fSSDStation(0)
+  {
+    this->SetGDMLFile(fname);
+  }
+  
+  //--------------------------------------------------------------------------------
+  
+  bool Geometry::SetGDMLFile(std::string fname)
+  {
+    if (fname.empty()) {
+      throw cet::exception("GeometrySetGDMLFile")
+	<< "cannot use empty string for GDML file, please fix this\n"
+	<< __FILE__ << ":" << __LINE__ << "\n";
+    }
+    
+    if (fname == fGDMLFile) return true;
+    
+    fGDMLFile = fname;
+    return this->LoadGDMLFile();
+  }
+  
+  //--------------------------------------------------------------------------------
+  
+  bool Geometry::LoadGDMLFile()
+  {
+    //    cet::search_path sp("FW_SEARCH_PATH");
+    
+    //    std::string fullPathGDMLFile;
+    
+    /*
+    if (!sp.find_file(fGDMLFile,fullPathGDMLFile) ) {
+      throw cet::exception("GeometryFileLoad")
+        << "cannot find GDML file " << fGDMLFile << " bail ungracefully\n"
+        << __FILE__ << ":" << __LINE__ << "\n";
+    }
+    */
+    mf::LogWarning("LoadNewGeometry") << "loading new geometry files\n"
+				      << fGDMLFile << "\n";
+    
+    int old_verbosity = gGeoManager->GetVerboseLevel();
+    
+    // TGeoManager is too verbose when loading geometry.
+    // Make it quiet.
+    gGeoManager->SetVerboseLevel(0);
+    
+    TGeoManager::Import(fGDMLFile.c_str());
+    
+    gGeoManager->SetVerboseLevel(old_verbosity);
+
+    const TGeoNode* world_n = (TGeoNode*)gGeoManager->GetTopNode();
+    const TGeoVolume* world_v = (TGeoVolume*)world_n->GetVolume();
+    
+    ExtractMagnetInfo(world_v);
+    ExtractRICHInfo(world_v);
+    ExtractSSDInfo(world_n);
+
     return true;
+  }
+  
+  //--------------------------------------------------------------------------------
+  
+  void Geometry::ExtractMagnetInfo(const TGeoVolume* world_v)
+  {
+    TGeoNode* magnet_n = (TGeoNode*)world_v->GetNode("magnet_phys");
+    TGeoVolume* magnet_v = (TGeoVolume*)magnet_n->GetVolume();
+    TGeoBBox* magnet_box = (TGeoBBox*)magnet_v->GetShape();
+    
+    double zcenter = magnet_n->GetMatrix()->GetTranslation()[2];
+    double dz = magnet_box->GetDZ();
+    
+    fMagnetUSZPos = zcenter-dz;
+    fMagnetDSZPos = zcenter-dz;
+    
+  }
+  
+  //--------------------------------------------------------------------------------
+  
+  void Geometry::ExtractRICHInfo(const TGeoVolume* world_v)
+  {
+    TGeoNode* rich_n = (TGeoNode*)world_v->GetNode("rich_phys");
+    TGeoVolume* rich_v = (TGeoVolume*)rich_n->GetVolume();
+    TGeoBBox* rich_box = (TGeoBBox*)rich_v->GetShape();
+    
+    double zcenter = rich_n->GetMatrix()->GetTranslation()[2];
+    double dz = rich_box->GetDZ();
+    
+    fRICHUSZPos = zcenter-dz;
+    fRICHDSZPos = zcenter-dz;
+    
+  }
 
-  return false;
+  //--------------------------------------------------------------------------------
+  
+  void Geometry::ExtractSSDInfo(const TGeoNode* world_n)
+  {
+    int nnodes = world_n->GetNodes()->GetEntries();
 
-}
+    const TGeoVolume* world_v = (TGeoVolume*)world_n->GetVolume();
+    
+    std::vector<std::string> nodeName;
+    
+    std::string sString = "ssdStation";
+    
+    for (int i=0; i<nnodes; ++i) {
+      std::string name = world_v->GetNode(i)->GetName();
+      if (name.find(sString) != std::string::npos)
+	nodeName.push_back(name);
+    }
 
-////////////////////////////////////////////////////////////////////////
+    fNSSDStations = (int)nodeName.size();
+    
+    for (auto name : nodeName) {
+      SSDStation st;
+      TGeoNode* st_n = (TGeoNode*)world_v->GetNode(name.c_str());
+      TGeoVolume* st_v = (TGeoVolume*)st_n->GetVolume();
+      TGeoBBox* st_box = (TGeoBBox*)st_v->GetShape();
+
+      // first add basic info about the SSD station
+      st.SetName(name);
+      st.SetDz(st_box->GetDZ());
+      st.SetPos(st_n->GetMatrix()->GetTranslation());
+      st.SetWidth(2*st_box->GetDX());
+      st.SetHeight(2*st_box->GetDY());
+
+      // now add individual SSDs to the station
+    }
+    
+  }
+
+} // end namespace geo
--- Geometry/Geometry.h
+++ Geometry/Geometry.h
@@ -1,29 +1,124 @@
+////////////////////////////////////////////////////////////////////////
 /// \file    Geometry.h
 /// \brief
-/// \version $Id: Geometry.h
+/// \version 
 /// \author  jpaley@fnal.gov
 ////////////////////////////////////////////////////////////////////////
 
-#include <string>
+#ifndef GEO_GEOMETRY_H
+#define GEO_GEOMETRY_H
 
-#include "TGeoManager.h"
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+class TGeoNode;
+class TGeoVolume;
+class TGeoMaterial;
+class TGeoManager;
+class TVector3;
 
 namespace geo {
-  class Geometry {
-    
+
+  class SSD {
   public:
-    Geometry() {};
-    Geometry(std::string fname) { LoadFromGDML(fname); }
-    ~Geometry() {};
+    SSD();
+    ~SSD() {};
+
+    std::string Name() { return fName;}
+    TVector3 Pos() { return fPos;}
+    double Dz() { return fDz;}
+    double Width() { return fWidth;}
+    double Height() { return fHeight;}
     
-    bool LoadFromGDML(std::string gdmlFile);
+    void SetName(std::string n) {fName = n; }
+    void SetPos(TVector3 pos) {fPos = pos;}
+    void SetDz(double dz) {fDz = dz;}
+    void SetWidth(double w) {fWidth = w;}
+    void SetHeight(double h) {fHeight = h;}
     
-    TGeoManager* RootGeoManager() const { return _geom; }
-    
-  private:
-    
-    TGeoManager* _geom;
-    
+  private:    
+    std::string fName;
+    TVector3 fPos;
+    double fDz;
+    double fWidth;
+    double fHeight;
   };
 
-} // end namespace geo
+  class SSDStation {
+  public:
+    SSDStation();
+    ~SSDStation() {};
+
+    void SetName(std::string n) {fName = n; }
+    void SetPos(TVector3 pos) {fPos = pos;}
+    void SetDz(double dz) {fDz = dz;}
+    void SetWidth(double w) {fWidth = w;}
+    void SetHeight(double h) {fHeight = h;}
+
+    std::string Name() { return fName; }
+    TVector3 Pos() {return fPos;}
+
+    int NSSDs() const {return (int)fSSD.size(); };
+    SSD GetSSD(int i) {return fSSD[i]; }
+    void AddSSD(SSD ssd) {fSSD.push_back(ssd); }
+    double Dz() { return fDz;}
+    double Width() { return fWidth; }
+    double Height() {return fHeight; }
+    
+  private:
+    std::string fName;
+    TVector3 fPos;
+    double fDz;
+    double fWidth;
+    double fHeight;
+    std::vector<geo::SSD> fSSD;
+  };
+  
+  class Geometry {
+  public:
+    Geometry(std::string fname);
+    ~Geometry() {};
+    
+    bool SetGDMLFile(std::string fname);
+    
+    double MagnetUSZPos() const {return fMagnetUSZPos; }
+    double MagnetDSZPos() const {return fMagnetDSZPos; }
+    double RICHUSZPos() const {return fMagnetUSZPos; }
+    double RICHDSZPos() const {return fMagnetDSZPos; }
+    
+    int NSSDStations() const { return fNSSDStations; }
+    int NSSDs() const { return fNSSDs; }
+    
+    SSDStation GetSSDStation(int i) {return fSSDStation[i]; }
+    
+    //    TGeoMaterial* Material(double x, double y, double z) const;
+    
+    std::string GDMLFile() const {return fGDMLFile; }
+    
+    TGeoManager* ROOTGeoManager() const;
+    
+  private:
+    Geometry();
+    
+    bool LoadGDMLFile();
+    void ExtractMagnetInfo(const TGeoVolume* v);
+    void ExtractRICHInfo(const TGeoVolume* v);
+    void ExtractSSDInfo(const TGeoNode* n);
+    
+    std::string fGDMLFile;
+
+    int    fNSSDStations;
+    int    fNSSDs;
+    double fMagnetUSZPos;
+    double fMagnetDSZPos;
+    double fRICHUSZPos;
+    double fRICHDSZPos;
+    std::vector<geo::SSDStation> fSSDStation;
+    
+  };
+  
+}
+
+#endif
