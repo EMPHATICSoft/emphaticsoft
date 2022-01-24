85de0dad96289049838e6468e007fc9bac68680d
Teresa Lackey <lackey32@fnal.gov>
2022-01-23 18:03:26 -0600

    Added into namespace emph to match Waveform class.

--- RawData/TRB3RawDigit.cxx
+++ RawData/TRB3RawDigit.cxx
@@ -5,29 +5,31 @@
 #include "RawData/TRB3RawDigit.h"
 
 #include <iostream>
+namespace emph {
 
-namespace rawdata{
+  namespace rawdata{
 
-  //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
 
-  TRB3RawDigit::TRB3RawDigit():
-    fgpa_header_word(0),
-    tdc_header_word(0),
-    tdc_epoch_word(0),
-    tdc_measurement_word(0),
-    event_index(0)
-  {
-  }
+    TRB3RawDigit::TRB3RawDigit():
+      fgpa_header_word(0),
+      tdc_header_word(0),
+      tdc_epoch_word(0),
+      tdc_measurement_word(0),
+      event_index(0)
+    {
+    }
 
-  //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
 
-  TRB3RawDigit::TRB3RawDigit(uint32_t fpga, uint32_t header, uint32_t epoch, uint32_t measurement):
-    fgpa_header_word(fpga),
-    tdc_header_word(header),
-    tdc_epoch_word(epoch),
-    tdc_measurement_word(measurement),
-    event_index(0)
-  {
-  }
+    TRB3RawDigit::TRB3RawDigit(uint32_t fpga, uint32_t header, uint32_t epoch, uint32_t measurement):
+      fgpa_header_word(fpga),
+      tdc_header_word(header),
+      tdc_epoch_word(epoch),
+      tdc_measurement_word(measurement),
+      event_index(0)
+    {
+    }
 
-}
+  } // end namespace rawdata
+} // end namespace emph
--- RawData/TRB3RawDigit.h
+++ RawData/TRB3RawDigit.h
@@ -5,50 +5,53 @@
 #include <iostream>
 #include <inttypes.h>
 
-namespace rawdata {
-  class TRB3RawDigit {
-  public:
-    TRB3RawDigit(); // Default constructor
-    virtual ~TRB3RawDigit() {}; // Destructor
+namespace emph {
 
-  private:
-    /// Found fields to hold the header, measurement error words.
-    uint32_t fgpa_header_word;
-    uint32_t tdc_header_word;
-    uint32_t tdc_epoch_word;
-    uint32_t tdc_measurement_word;
-    int event_index;
+  namespace rawdata {
+    class TRB3RawDigit {
+    public:
+      TRB3RawDigit(); // Default constructor
+      virtual ~TRB3RawDigit() {}; // Destructor
 
-  public:
-    TRB3RawDigit(uint32_t fpga, uint32_t header, uint32_t epoch, uint32_t measurement);
+    //private:
+      /// Found fields to hold the header, measurement error words.
+      uint32_t fgpa_header_word;
+      uint32_t tdc_header_word;
+      uint32_t tdc_epoch_word;
+      uint32_t tdc_measurement_word;
+      int event_index;
 
-    /// Is this the leading edge measurement?
-    bool IsLeading() const {return 0;}
-    /// Is this the trailing edge measurement?
-    bool IsTrailing() const {return 0;}
+    public:
+      TRB3RawDigit(uint32_t fpga, uint32_t header, uint32_t epoch, uint32_t measurement);
 
-    uint32_t GetBoardId() const {return fgpa_header_word & 0xf;}
+      /// Is this the leading edge measurement?
+      bool IsLeading() const {return 0;}
+      /// Is this the trailing edge measurement?
+      bool IsTrailing() const {return 0;}
 
-    /// Get the TDC measurement
-    uint32_t GetMeasurement() const {
-      return (tdc_measurement_word & 0x7ffff);
-    }
+      uint32_t GetBoardId() const {return fgpa_header_word & 0xf;}
 
-    uint32_t GetFineTime() const {
-      return (tdc_measurement_word & 0x1ff000) >> 12;
+      /// Get the TDC measurement
+      uint32_t GetMeasurement() const {
+        return (tdc_measurement_word & 0x7ffff);
+      }
+
+      uint32_t GetFineTime() const {
+        return (tdc_measurement_word & 0x1ff000) >> 12;
+      };
+
+      uint32_t GetCoarseTime() const {return tdc_measurement_word & 0x7ff;};
+
+      // This epoch counter rolls every 10us
+      uint32_t GetEpochCounter() const {return tdc_epoch_word & 0xfffffff;};
+
+      /// Get the channel number
+      uint32_t GetChannel() const {
+        return ((tdc_measurement_word & 0xfc00000 ) >> 22 );
+      }
+
     };
 
-    uint32_t GetCoarseTime() const {return tdc_measurement_word & 0x7ff;};
-
-    // This epoch counter rolls every 10us
-    uint32_t GetEpochCounter() const {return tdc_epoch_word & 0xfffffff;};
-
-    /// Get the channel number
-    uint32_t GetChannel() const {
-      return ((tdc_measurement_word & 0xfc00000 ) >> 22 );
-    }
-
-  };
-}
-
+  } // end namespace rawdata
+} // end namespace emph
 #endif
