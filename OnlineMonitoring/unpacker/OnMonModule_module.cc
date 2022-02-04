dc8fce2af9905e8d5216e0659e6317128146c050
Tersea Lackey <lackey32@fnal.gov>
2022-02-04 14:11:56 -0600

    Fixed typo in include.

--- OnlineMonitoring/unpacker/OnMonModule_module.cc
+++ OnlineMonitoring/unpacker/OnMonModule_module.cc
@@ -26,7 +26,7 @@
 // EMPHATICSoft includes
 #include "RawData/TRB3RawDigit.h"
 #include "RawData/SSDRawDigit.h"
-#include "RawData/Waveform.h"
+#include "RawData/WaveForm.h"
 #include "Geometry/DetectorDefs.h"
 
 using namespace emph;
@@ -36,20 +36,20 @@ namespace emph {
   namespace onmon {
     class OnMonModule : public art::EDAnalyzer {
     public:
-      explicit OnMonModule(fhicl::ParameterSet const& pset); // Required! explicit tag tells the compiler this is not a copy constructor      
-      ~OnMonModule();                        
-      
+      explicit OnMonModule(fhicl::ParameterSet const& pset); // Required! explicit tag tells the compiler this is not a copy constructor
+      ~OnMonModule();
+
       // Optional, read/write access to event
-      void analyze(const art::Event& evt); 
-      
+      void analyze(const art::Event& evt);
+
       // Optional if you want to be able to configure from event display, for example
       void reconfigure(const fhicl::ParameterSet& pset);
-      
+
       // Optional use if you have histograms, ntuples, etc you want around for every event
       void beginJob();
-      
+
     private:
-      TH2I*  fNRawObjectsHisto;  
+      TH2I*  fNRawObjectsHisto;
       TH1I*  fNTriggerVsDet;
       void   FillGasCkovPlots(art::Handle< std::vector<rawdata::WaveForm> > &);
       void   FillBACkovPlots(art::Handle< std::vector<rawdata::WaveForm> > &);
@@ -69,18 +69,18 @@ namespace emph {
       void   MakeLGCaloPlots();
       void   MakeRPCPlots();
       void   MakeTrigPlots();
-      
+
     };
-    
+
     //.......................................................................
-    OnMonModule::OnMonModule(fhicl::ParameterSet const& pset) 
+    OnMonModule::OnMonModule(fhicl::ParameterSet const& pset)
       : EDAnalyzer(pset)
     {
-      
+
       this->reconfigure(pset);
-      
+
     }
-    
+
     //......................................................................
     OnMonModule::~OnMonModule()
     {
@@ -88,13 +88,13 @@ namespace emph {
       // Clean up any memory allocated by your module
       //======================================================================
     }
-    
+
     //......................................................................
     void OnMonModule::reconfigure(const fhicl::ParameterSet& )// pset)
     {
-      
+
     }
-    
+
     //......................................................................
     void OnMonModule::beginJob()
     {
@@ -102,14 +102,14 @@ namespace emph {
       // Book histograms, ntuples, initialize counts etc., etc., ...
       //
       art::ServiceHandle<art::TFileService> tfs;
-      
+
       fNRawObjectsHisto = tfs->make<TH2I>("NRawObjectsHisto",
 					  "Number of Raw Objects Per Detector",
 					  emph::geo::NDetectors+1,0,emph::geo::NDetectors+1,
 					  100,0,100);
-      
+
       fNTriggerVsDet = tfs->make<TH1I>("NTriggerVsDet","Number of Triggers Seen by Each Detector",emph::geo::NDetectors+1,0,emph::geo::NDetectors+1);
-      
+
       // label x-axis
       std::string labelStr;
       int i=0;
@@ -134,7 +134,7 @@ namespace emph {
       MakeTrigPlots();
 
     }
-    
+
     //......................................................................
 
     void  OnMonModule::MakeGasCkovPlots()
@@ -206,13 +206,13 @@ namespace emph {
     void OnMonModule::FillSSDPlots(art::Handle< std::vector<emph::rawdata::SSDRawDigit> > & )
     {
     }
-    
+
     //......................................................................
 
     void OnMonModule::FillARICHPlots(art::Handle< std::vector<rawdata::TRB3RawDigit> > & )
     {
     }
-    
+
     //......................................................................
 
     void    OnMonModule::FillLGCaloPlots(art::Handle< std::vector<rawdata::WaveForm> > & )
@@ -230,19 +230,19 @@ namespace emph {
     void   OnMonModule::FillTrigPlots(art::Handle< std::vector<rawdata::WaveForm> > & )
     {
     }
-    
+
     //......................................................................
     void OnMonModule::analyze(const art::Event& evt)
     {
       std::string labelStr;
 
       for (int i=0; i<emph::geo::NDetectors; ++i) {
-	
-	labelStr = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(i));	
+
+	labelStr = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(i));
 	art::Handle< std::vector<emph::rawdata::WaveForm> > wfHandle;
 	try {
 	  evt.getByLabel(labelStr, wfHandle);
-	  
+
 	  if (!wfHandle->empty()) {
 	    fNRawObjectsHisto->Fill(i,wfHandle->size());
 	    fNTriggerVsDet->Fill(i);
@@ -264,22 +264,22 @@ namespace emph {
 	      }
 	      catch(...) {
 		std::cout << "No TRB3 digits found for the T0!" << std::endl;
-	      }	    
+	      }
 	    }
-	    
+
 	  }
 	}
 	catch(...) {
 	  //	  std::cout << "Nothing found in " << labelStr << std::endl;
-	}	
+	}
       }
       // get TRB3digits
       int i = emph::geo::RPC;
-      labelStr = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(i));	
+      labelStr = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(i));
       art::Handle< std::vector<emph::rawdata::TRB3RawDigit> > trbHandle;
       try {
 	evt.getByLabel(labelStr, trbHandle);
-      
+
 	if (!trbHandle->empty()) {
 	  fNRawObjectsHisto->Fill(i,trbHandle->size());
 	  fNTriggerVsDet->Fill(i);
@@ -287,15 +287,15 @@ namespace emph {
 	}
       }
       catch(...) {
-	
+
       }
       // get SSDdigits
       i = emph::geo::SSD;
-      labelStr = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(i));	
+      labelStr = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(i));
       art::Handle< std::vector<emph::rawdata::SSDRawDigit> > ssdHandle;
       try {
 	evt.getByLabel(labelStr, ssdHandle);
-      
+
 	if (!ssdHandle->empty()) {
 	  fNRawObjectsHisto->Fill(i,ssdHandle->size());
 	  fNTriggerVsDet->Fill(i);
@@ -303,9 +303,9 @@ namespace emph {
 	}
       }
       catch(...) {
-	
+
       }
-      
+
       return;
     }
   }
