8e9937d86ae4e6c68929373daba59abcb5db7c7c
jonpaley <jonpaley>
2022-02-11 10:37:47 -0600

    Add EventDisplayBase package

---
+++ EventDisplayBase/EventDisplay_service.cc
@@ -0,0 +1,17 @@
+///
+/// \file  EventDisplay_service.cc
+/// \brief The interactive event display
+///
+/// \version $Id: EventDisplay.cxx,v 1.25 2012-03-05 20:37:14 brebel Exp $
+/// \author  messier@indiana.edu
+///
+#include "nuevdb/EventDisplayBase/EventDisplay.h"
+#include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
+
+namespace evdb {
+
+  DEFINE_ART_SERVICE(EventDisplay)
+
+} // namespace evdb
+
+////////////////////////////////////////////////////////////////////////
