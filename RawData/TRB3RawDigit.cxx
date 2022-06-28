72c3e3baf2afb57e63b3c34ca4036baf69cf0d0a
Arturo Fiorentini <arturo@arturo.fnal.gov>
2022-06-28 14:43:42 -0500

    Moved function implementation to cxx file.

--- RawData/TRB3RawDigit.cxx
+++ RawData/TRB3RawDigit.cxx
@@ -31,5 +31,60 @@ namespace emph {
     {
     }
 
+    //----------------------------------------------------------------------
+
+    uint32_t TRB3RawDigit::GetBoardId() const {
+      uint32_t id = 99;
+      if      ((fpga_header_word & 0xffff)==0x100) id=0;
+      else if ((fpga_header_word & 0xffff)==0x101) id=1;
+      else if ((fpga_header_word & 0xffff)==0x102) id=2;
+      else if ((fpga_header_word & 0xffff)==0x103) id=3;
+      else if ((fpga_header_word & 0xffff)==0x200) id=4;
+      else if ((fpga_header_word & 0xffff)==0x201) id=5;
+      else if ((fpga_header_word & 0xffff)==0x202) id=6;
+      else if ((fpga_header_word & 0xffff)==0x203) id=7;
+      else if ((fpga_header_word & 0xffff)==0x300) id=8;
+      else if ((fpga_header_word & 0xffff)==0x301) id=9;
+      else if ((fpga_header_word & 0xffff)==0x302) id=10;
+      else if ((fpga_header_word & 0xffff)==0x303) id=11;
+      else if ((fpga_header_word & 0xffff)==0x400) id=12;
+      else if ((fpga_header_word & 0xffff)==0x401) id=13;
+      else if ((fpga_header_word & 0xffff)==0x402) id=14;
+      else if ((fpga_header_word & 0xffff)==0x403) id=15;
+      else if ((fpga_header_word & 0xffff)==0x500) id=16;
+      else if ((fpga_header_word & 0xffff)==0x501) id=17;
+      else if ((fpga_header_word & 0xffff)==0x502) id=18;
+      else if ((fpga_header_word & 0xffff)==0x503) id=19;
+      else std::cout << "fpga " << std::hex << fpga_header_word << " (hex) "
+                       << std::dec << fpga_header_word << " (dec) "
+                       << "is not supported" << std::endl;
+      return id;
+    }
+
+    //----------------------------------------------------------------------
+
+    double TRB3RawDigit::GetFinalTime() const {
+
+      // default linear calibration constants
+      double trb3LinearLowEnd = 17.0;
+      double trb3LinearHighEnd = 473.0;
+
+      // T0
+      if (GetBoardId()==18) {
+        trb3LinearLowEnd = 15.0;
+        trb3LinearHighEnd = 494.0;
+      }
+      // RPC
+      else if (GetBoardId()==19) {
+        trb3LinearLowEnd = 15.0;
+        trb3LinearHighEnd = 476.0;
+      }
+
+      return ((double)GetEpochCounter())*10240026.0
+           + ((double) GetCoarseTime())*5000.0
+           - ((((double)GetFineTime())-trb3LinearLowEnd)/(trb3LinearHighEnd-trb3LinearLowEnd))*5000.0;
+    }
+
+
   } // end namespace rawdata
 } // end namespace emph
