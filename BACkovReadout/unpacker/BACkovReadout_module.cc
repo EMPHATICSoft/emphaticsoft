e9d2c6281a559e0e34369dc4a1c131e72ea98b9b
Jack Mirabito <jmirabit@emphaticgpvm02.fnal.gov>
2022-07-08 10:50:23 -0500

    Added LGCalo waveforms and fixed channel mapping

--- BACkovReadout/unpacker/BACkovReadout_module.cc
+++ BACkovReadout/unpacker/BACkovReadout_module.cc
@@ -1,12 +1,13 @@
 ////////////////////////////////////////////////////////////////////////
-/// \brief   Analyzer module to create online monitoring plots
-/// \author  $Author: jpaley $
+/// \brief   Module to readout BACkov, Gas Cherenkov Waveforms
+/// \author  $Author: jmirabit $
 ////////////////////////////////////////////////////////////////////////
 // C/C++ includes
 #include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdio>
 
 // ROOT includes
 #include "TFile.h"
@@ -50,7 +51,7 @@ namespace emph {
       void endJob();
 
     private:
-      void   FillBACkovPlots(art::Handle< std::vector<rawdata::WaveForm> > &,art::Handle< std::vector<rawdata::WaveForm> > &,art::Handle< std::vector<rawdata::WaveForm> > &);
+      void   FillBACkovPlots(art::Handle< std::vector<rawdata::WaveForm> > &,art::Handle< std::vector<rawdata::WaveForm> > &,art::Handle< std::vector<rawdata::WaveForm> > &,art::Handle< std::vector<rawdata::WaveForm> > &);
       
       emph::cmap::ChannelMap* fChannelMap;
       std::string fChanMapFileName;
@@ -71,16 +72,19 @@ namespace emph {
       bool fMakeBACkovPlots;
 		TFile* f; 
 		TTree *tree;
-		int event, chan, Tchan, Gchan;
+		int event;
 		float Bq0, Bq1, Bq2, Bq3, Bq4, Bq5;
 		float Bmax0, Bmax1, Bmax2, Bmax3, Bmax4, Bmax5;
 		float Bblw0, Bblw1, Bblw2, Bblw3, Bblw4, Bblw5;
-		float Tq9, Tq10, Tq11, Tq12;
-		float Tmax9, Tmax10, Tmax11, Tmax12;
-		float Tblw9, Tblw10, Tblw11, Tblw12;
+		float Tq5, Tq6, Tq15, Tq16;
+		float Tmax5, Tmax6, Tmax15, Tmax16;
+		float Tblw5, Tblw6, Tblw15, Tblw16;
 		float Gq0, Gq1, Gq2;
 		float Gmax0, Gmax1, Gmax2;
 		float Gblw0, Gblw1, Gblw2;
+		float LGq0, LGq1, LGq2, LGq3, LGq4, LGq5, LGq6, LGq7, LGq8;
+		float LGblw0, LGblw1, LGblw2, LGblw3, LGblw4, LGblw5, LGblw6, LGblw7, LGblw8;
+		
     };
 
     //.......................................................................
@@ -123,12 +127,11 @@ namespace emph {
 	}
 	std::cout << "Loaded channel map from file " << fChanMapFileName << std::endl;
       }
-   
-		char filename[32];
+      char filename[32];
       sprintf(filename,"BACkov.root");
       f = new TFile(filename,"RECREATE");
-   
-		tree=new TTree("BACkovtree","");
+
+      		tree=new TTree("BACkovtree","");
 
 		tree->Branch("event",&event);
 
@@ -151,18 +154,18 @@ namespace emph {
 		tree->Branch("BAC_blw4",&Bblw4);
 		tree->Branch("BAC_blw5",&Bblw5);
 
-		tree->Branch("T0_q9",&Tq9);
-		tree->Branch("T0_max9",&Tmax9);
-		tree->Branch("T0_blw9",&Tblw9);
-		tree->Branch("T0_q10",&Tq10);
-		tree->Branch("T0_max10",&Tmax10);
-		tree->Branch("T0_blw10",&Tblw10);
-		tree->Branch("T0_q11",&Tq11);
-		tree->Branch("T0_max11",&Tmax11);
-		tree->Branch("T0_blw11",&Tblw11);
-		tree->Branch("T0_q12",&Tq12);
-		tree->Branch("T0_max12",&Tmax12);
-		tree->Branch("T0_blw12",&Tblw12);
+		tree->Branch("T0_q5",&Tq5);
+		tree->Branch("T0_max5",&Tmax5);
+		tree->Branch("T0_blw5",&Tblw5);
+		tree->Branch("T0_q6",&Tq6);
+		tree->Branch("T0_max6",&Tmax6);
+		tree->Branch("T0_blw6",&Tblw6);
+		tree->Branch("T0_q15",&Tq15);
+		tree->Branch("T0_max15",&Tmax15);
+		tree->Branch("T0_blw15",&Tblw15);
+		tree->Branch("T0_q16",&Tq16);
+		tree->Branch("T0_max16",&Tmax16);
+		tree->Branch("T0_blw16",&Tblw16);
 		
 		tree->Branch("GC_q0",&Gq0);
 		tree->Branch("GC_max0",&Gmax0);
@@ -173,142 +176,173 @@ namespace emph {
 		tree->Branch("GC_q2",&Gq2);
 		tree->Branch("GC_max2",&Gmax2);
 		tree->Branch("GC_blw2",&Gblw2);
-    }
-    
-    //......................................................................
 
+		tree->Branch("LG_q0",&LGq0);
+		tree->Branch("LG_q1",&LGq1);
+		tree->Branch("LG_q2",&LGq2);
+		tree->Branch("LG_q3",&LGq3);
+		tree->Branch("LG_q4",&LGq4);
+		tree->Branch("LG_q5",&LGq5);
+		tree->Branch("LG_q6",&LGq5);
+		tree->Branch("LG_q7",&LGq5);
+		tree->Branch("LG_q8",&LGq5);
+		tree->Branch("LG_blw0",&LGblw0);
+		tree->Branch("LG_blw1",&LGblw1);
+		tree->Branch("LG_blw2",&LGblw2);
+		tree->Branch("LG_blw3",&LGblw3);
+		tree->Branch("LG_blw4",&LGblw4);
+		tree->Branch("LG_blw5",&LGblw5);
+		tree->Branch("LG_blw6",&LGblw5);
+		tree->Branch("LG_blw7",&LGblw5);
+		tree->Branch("LG_blw8",&LGblw5);
+	    }
+
+    //......................................................................
     void BACkovReadout::endJob()
     {
+      char filename[32];
+      sprintf(filename,"BACkov_s%d_r%d.root",fRun,fSubrun);
 
-		tree->Write();
+      std::cout<<"Writing file for run/subrun: " << fRun << "/" <<fSubrun << std::endl;
+      std::rename("BACkov.root",filename);      
+      tree->Write();
       f->Close();
       delete f; f=0;
     }
     
     //......................................................................
 
-    void BACkovReadout::FillBACkovPlots(art::Handle< std::vector<emph::rawdata::WaveForm> > & wvfmH, art::Handle< std::vector<emph::rawdata::WaveForm> > & TwvfmH, art::Handle< std::vector<emph::rawdata::WaveForm> > & GwvfmH)
+    void BACkovReadout::FillBACkovPlots(art::Handle< std::vector<emph::rawdata::WaveForm> > & wvfmH, art::Handle< std::vector<emph::rawdata::WaveForm> > & TwvfmH, art::Handle< std::vector<emph::rawdata::WaveForm> > & GwvfmH,art::Handle< std::vector<emph::rawdata::WaveForm> > & LGwvfmH)
     {
       int nchan = emph::geo::DetInfo::NChannel(emph::geo::BACkov);
       emph::cmap::FEBoardType boardType = emph::cmap::V1720;
       emph::cmap::EChannel echan;
       emph::cmap::EChannel Techan;
       emph::cmap::EChannel Gechan;
+      emph::cmap::EChannel LGechan;
       echan.SetBoardType(boardType);
       Techan.SetBoardType(boardType);
       Gechan.SetBoardType(boardType);
+      LGechan.SetBoardType(boardType);
       if (fMakeBACkovPlots) {
-        //std::cout << "Making BACkov ADC OnMon plots" << std::endl;
-        for (int i=0; i<nchan; ++i) {
-          //sprintf(hname,"BACkovADC_%d",i);
-          //fBACkovADCDist[i] = h.GetTH1F(hname);
-        }
-        //std::cout << "Making BACkov WaveForm OnMon plots" << std::endl;
-        for (int i=0; i<nchan; ++i) {
-          //sprintf(hname,"BACkovWaveForm_%d",i);
-          //fBACkovWaveForm.push_back(h.GetTH1F(hname));
-          //fBACkovWaveForm[i]->SetBit(TH1::kIsAverage);
-          fNEventsBACkov.push_back(0);
-          }
-
 	if (!wvfmH->empty()) {
 	  for (size_t idx=0; idx < wvfmH->size(); ++idx) {
 	    const rawdata::WaveForm& wvfm = (*wvfmH)[idx];
-	    chan = wvfm.Channel();
+	    int chan = wvfm.Channel();
 	    int board = wvfm.Board();
 	    event = fNEvents;
             echan.SetBoard(board);
             echan.SetChannel(chan);
             emph::cmap::DChannel dchan = fChannelMap->DetChan(echan);
             int detchan = dchan.Channel();
-            if (detchan >= 0 && detchan < nchan) {
+	    //std::cout<<"BACkov Board, Chan, detchan : "<<board<<", "<<chan<<", "<<detchan<<std::endl;
+	    if (detchan >=0 && detchan < nchan){
 	      if (detchan==0){
-		Bq0 = wvfm.Charge();
-	        Bmax0 = wvfm.Baseline()-wvfm.PeakADC();
-	        Bblw0 = wvfm.BLWidth();
+	        Bq0 = wvfm.Charge(); Bmax0 = wvfm.Baseline()-wvfm.PeakADC(); Bblw0 = wvfm.BLWidth();
 	      }
 	      if (detchan==1){
-		Bq1 = wvfm.Charge();
-	        Bmax1 = wvfm.Baseline()-wvfm.PeakADC();
-	        Bblw1 = wvfm.BLWidth();
+	        Bq1 = wvfm.Charge(); Bmax1 = wvfm.Baseline()-wvfm.PeakADC(); Bblw1 = wvfm.BLWidth();
 	      }
 	      if (detchan==2){
-		Bq2 = wvfm.Charge();
-	        Bmax2 = wvfm.Baseline()-wvfm.PeakADC();
-	        Bblw2 = wvfm.BLWidth();
+	        Bq2 = wvfm.Charge(); Bmax2 = wvfm.Baseline()-wvfm.PeakADC(); Bblw2 = wvfm.BLWidth();
 	      }
 	      if (detchan==3){
-		Bq3 = wvfm.Charge();
-	        Bmax3 = wvfm.Baseline()-wvfm.PeakADC();
-	        Bblw3 = wvfm.BLWidth();
+	        Bq3 = wvfm.Charge(); Bmax3 = wvfm.Baseline()-wvfm.PeakADC(); Bblw3 = wvfm.BLWidth();
 	      }
 	      if (detchan==4){
-		Bq4 = wvfm.Charge();
-	        Bmax4 = wvfm.Baseline()-wvfm.PeakADC();
-	        Bblw4 = wvfm.BLWidth();
+	        Bq4 = wvfm.Charge(); Bmax4 = wvfm.Baseline()-wvfm.PeakADC(); Bblw4 = wvfm.BLWidth();
 	      }
 	      if (detchan==5){
-		Bq5 = wvfm.Charge();
-	        Bmax5 = wvfm.Baseline()-wvfm.PeakADC();
-	        Bblw5 = wvfm.BLWidth();
-	      } 
-            }
+	        Bq5 = wvfm.Charge(); Bmax5 = wvfm.Baseline()-wvfm.PeakADC(); Bblw5 = wvfm.BLWidth();
+	      }
+	    }
 
 	  }
 	  for (size_t idx=0; idx<TwvfmH->size(); ++idx){
 	    const rawdata::WaveForm& Twvfm = (*TwvfmH)[idx];
-	    Tchan = Twvfm.Channel();
+	    int Tchan = Twvfm.Channel();
 	    int Tboard = Twvfm.Board();
             Techan.SetBoard(Tboard);
             Techan.SetChannel(Tchan);
             emph::cmap::DChannel Tdchan = fChannelMap->DetChan(Techan);
             int Tdetchan = Tdchan.Channel();
-            if (Tdetchan==9){
-	      Tq9 = Twvfm.Charge();
-	      Tmax9 = Twvfm.Baseline()-Twvfm.PeakADC();
-	      Tblw9 = Twvfm.BLWidth();
-	      std::cout<<"Tboard is: "<<Tboard<<"     and Tchan is: "<<Tchan<<std::endl;	
+	    //std::cout<<"T0 Board, Chan, detchan : "<<Tboard<<", "<<Tchan<<", "<<Tdetchan<<std::endl;
+            if (Tdetchan==5){
+	      Tq5 = Twvfm.Charge(); Tmax5 = Twvfm.Baseline()-Twvfm.PeakADC(); Tblw5 = Twvfm.BLWidth();
 	    }
-	    if (Tdetchan==10){
-	      Tq10 = Twvfm.Charge();
-	      Tmax10 = Twvfm.Baseline()-Twvfm.PeakADC();
-	      Tblw10 = Twvfm.BLWidth();	
+	    if (Tdetchan==6){
+	      Tq6 = Twvfm.Charge(); Tmax6 = Twvfm.Baseline()-Twvfm.PeakADC(); Tblw6 = Twvfm.BLWidth();	
 	    }
-	    if (Tdetchan==11){
-	      Tq11 = Twvfm.Charge();
-	      Tmax11 = Twvfm.Baseline()-Twvfm.PeakADC();
-	      Tblw11 = Twvfm.BLWidth();	
+	    if (Tdetchan==15){
+	      Tq15 = Twvfm.Charge(); Tmax15 = Twvfm.Baseline()-Twvfm.PeakADC(); Tblw15 = Twvfm.BLWidth();	
 	    }
-	    if (Tdetchan==12){
-	      Tq12 = Twvfm.Charge();
-	      Tmax12 = Twvfm.Baseline()-Twvfm.PeakADC();
-	      Tblw12 = Twvfm.BLWidth();	
+	    if (Tdetchan==16){
+	      Tq16 = Twvfm.Charge(); Tmax16 = Twvfm.Baseline()-Twvfm.PeakADC(); Tblw16 = Twvfm.BLWidth();	
 	    }
 	  } 
+
 	  for (size_t idx=0; idx<GwvfmH->size(); ++idx){
 	    const rawdata::WaveForm& Gwvfm = (*GwvfmH)[idx];
-	    Gchan = Gwvfm.Channel();
+	    int Gchan = Gwvfm.Channel();
 	    int Gboard = Gwvfm.Board();
             Gechan.SetBoard(Gboard);
             Gechan.SetChannel(Gchan);
             emph::cmap::DChannel Gdchan = fChannelMap->DetChan(Gechan);
             int Gdetchan = Gdchan.Channel();
+	    //std::cout<<"GC Board, Chan, detchan : "<<Gboard<<", "<<Gchan<<", "<<Gdetchan<<std::endl;
             if (Gdetchan==0){
-	      Gq0 = Gwvfm.Charge();
-;	      Gmax0 = Gwvfm.Baseline()-Gwvfm.PeakADC();
-	      Gblw0 = Gwvfm.BLWidth();
+	      Gq0 = Gwvfm.Charge(); Gmax0 = Gwvfm.Baseline()-Gwvfm.PeakADC(); Gblw0 = Gwvfm.BLWidth();
 	    }
 	    if (Gdetchan==1){
-	      Gq1 =  Gwvfm.Charge();
-	      Gmax1 = Gwvfm.Baseline()-Gwvfm.PeakADC();
-	      Gblw1 = Gwvfm.BLWidth();
+	      Gq1 =  Gwvfm.Charge(); Gmax1 = Gwvfm.Baseline()-Gwvfm.PeakADC(); Gblw1 = Gwvfm.BLWidth();
 	    }
 	    if (Gdetchan==2){
-	      Gq2 = Gwvfm.Charge();
-	      Gmax2 = Gwvfm.Baseline()-Gwvfm.PeakADC();
-	      Gblw2 = Gwvfm.BLWidth();
+	      Gq2 = Gwvfm.Charge(); Gmax2 = Gwvfm.Baseline()-Gwvfm.PeakADC(); Gblw2 = Gwvfm.BLWidth();
 	    }
 	  } 
+
+          for (size_t idx=0; idx<LGwvfmH->size(); ++idx){
+	    const rawdata::WaveForm& LGwvfm = (*LGwvfmH)[idx];
+	    int LGchan = LGwvfm.Channel();
+	    int LGboard = LGwvfm.Board();
+            LGechan.SetBoard(LGboard);
+            LGechan.SetChannel(LGchan);
+            emph::cmap::DChannel LGdchan = fChannelMap->DetChan(LGechan);
+            int LGdetchan = LGdchan.Channel();
+	    //std::cout<<"LGCalo Board, Chan, detchan : "<<LGboard<<", "<<LGchan<<", "<<LGdetchan<<std::endl;
+            if (LGdetchan==0){
+	      LGq0 = 100;  LGblw0 = LGwvfm.BLWidth();
+	    }
+	    if (LGdetchan==1){
+	      LGq1 = LGwvfm.Charge(); LGblw1 = LGwvfm.BLWidth();
+	    }
+	    if (LGdetchan==2){
+	      LGq2 = LGwvfm.Charge();  LGblw2 = LGwvfm.BLWidth();
+	    }
+	    if (LGdetchan==3){
+	      LGq3 = LGwvfm.Charge();  LGblw3 = LGwvfm.BLWidth();
+	    }
+	    if (LGdetchan==4){
+	      LGq4 = LGwvfm.Charge();  LGblw4 = LGwvfm.BLWidth();
+	    }  
+	    if (LGdetchan==5){
+	      LGq5 = LGwvfm.Charge();  LGblw5 = LGwvfm.BLWidth();
+	    } 
+	    if (LGdetchan==6){
+	      LGq6 = LGwvfm.Charge();  LGblw6 = LGwvfm.BLWidth();
+	    }
+	    if (LGdetchan==7){
+	      LGq7 = LGwvfm.Charge();  LGblw7 = LGwvfm.BLWidth();
+	    }  
+	    if (LGdetchan==8){
+	      LGq8 = LGwvfm.Charge();  LGblw8 = LGwvfm.BLWidth();
+	    } 
+
+
+	  }	
+
+
+
 	  tree->Fill();
       }
     }
@@ -321,7 +355,7 @@ namespace emph {
       ++fNEvents;
       fRun = evt.run();
       fSubrun = evt.subRun();     
-      std::string labelStrBAC, labelStrT0, labelStrGC;
+      std::string labelStrBAC, labelStrT0, labelStrGC, labelStrLG;
 
       // get WaveForm
       int i = emph::geo::BACkov;
@@ -336,13 +370,19 @@ namespace emph {
       labelStrGC = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(k));
       art::Handle< std::vector<emph::rawdata::WaveForm> > wfHandleGC;
 
+      int l = emph::geo::LGCalo;
+      labelStrLG = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(l));
+      art::Handle< std::vector<emph::rawdata::WaveForm> > wfHandleLG;
+
+
       try {
 	evt.getByLabel(labelStrBAC, wfHandleBAC);
 	evt.getByLabel(labelStrT0, wfHandleT0);
 	evt.getByLabel(labelStrGC, wfHandleGC);
+	evt.getByLabel(labelStrLG, wfHandleLG);
 
 	if (!wfHandleBAC->empty()) {
-	  FillBACkovPlots(wfHandleBAC, wfHandleT0, wfHandleGC);
+	  FillBACkovPlots(wfHandleBAC, wfHandleT0, wfHandleGC, wfHandleLG);
 	}
       }
       catch(...) {
