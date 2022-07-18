6e6eea96f6c74fe796f0d0b51d57d76d6e148e4a
Shunsuke Kajikawa <kajikawa@lambda.phys.tohoku.ac.jp>
2022-07-18 16:14:19 -0500

    Modified files for compilation. This work is in progress. This code does not work yet when compiled.

--- T0Ana/T0Ana_module.cc
+++ T0Ana/T0Ana_module.cc
@@ -30,72 +30,81 @@
 #include "RawData/WaveForm.h"
 #include "RawData/TRB3RawDigit.h"
 
+// Define parameters of T0 detector
+#define N_SEG 10
+#define N_CH_DET 20
 
 using namespace emph;
 
 ///package to illustrate how to write modules
 namespace emph {
-    class T0Ana : public art::EDAnalyzer {
-    public:
-      explicit T0Ana(fhicl::ParameterSet const& pset); // Required! explicit tag tells the compiler this is not a copy constructor
-      ~T0Ana();
+  class T0Ana : public art::EDAnalyzer {
+  public:
+    explicit T0Ana(fhicl::ParameterSet const& pset); // Required! explicit tag tells the compiler this is not a copy constructor
+    ~T0Ana();
 
-      // Optional, read/write access to event
-      void analyze(const art::Event& evt);
+    // Optional, read/write access to event
+    void analyze(const art::Event& evt);
 
-      // Optional if you want to be able to configure from event display, for example
-      void reconfigure(const fhicl::ParameterSet& pset);
+    // Optional if you want to be able to configure from event display, for example
+    void reconfigure(const fhicl::ParameterSet& pset);
 
-      // Optional use if you have histograms, ntuples, etc you want around for every event
-      void beginJob();
-      void endJob();
+    // Optional use if you have histograms, ntuples, etc you want around for every event
+    void beginJob();
+    void endJob();
 
-    private:
-      const int n_seg = 10;
-      const int n_ch = 41; // Number of all channels for T0
-      const int n_ch_det = 20; // Number of leading channels for T0
-      const int RPC_board = 1283;
-      const int T0_board = 1282;
+  private:
+    static const int n_seg = N_SEG;
+    static const int n_ch = 41; // Number of all channels for T0
+    static const int n_ch_det = N_CH_DET; // Number of leading channels for T0
+    static const int RPC_board = 1283;
+    static const int T0_board = 1282;
 
-      const double epoch_const = 10240026.0; // Constant for epochtime
-      const double coarse_const = 5000.0; // Constant for coarsetime
-      double trb3_linear_low = 15.0; // Calibration for low end
-      double trb3LinearHighEnd = 499.0; // Calibration for FPGA2 -- T0
-      double trb3LinearHighEnd_RPC = 491.0; // Calibration for FPGA3 -- RPC
-      // std::array<double, 2> trb3_linear_high = {499.0, 491.0}; // Calibration for FPGA2 -- T0 and FPGA3 -- RPC
+    const double epoch_const = 10240026.0; // Constant for epochtime
+    const double coarse_const = 5000.0; // Constant for coarsetime
+    double trb3_linear_low = 15.0; // Calibration for low end
+    double trb3_linear_high_T0 = 499.0; // Calibration for FPGA2 -- T0
+    double trb3_linear_high_RPC = 491.0; // Calibration for FPGA3 -- RPC
+    // std::array<double, 2> trb3_linear_high = {499.0, 491.0}; // Calibration for FPGA2 -- T0 and FPGA3 -- RPC
 
-      bool   FindSeg(int);
-      bool   FindTrailBot(int);
-      bool   FindTrailTop(int);
-      bool   FindLeadBot(int);
-      bool   FindLeadTop(int);
-      int    GetSegIdTrb3(int);
-      std::array<int, 2> GetSegChTrb3(int);
-      void   FillT0AnaTree(art::Handle< std::vector<rawdata::WaveForm> > &,
-			   art::Handle< std::vector<rawdata::TRB3RawDigit> > &);
-      std::array<double, n_ch_det> GetTot(const std::vector<rawdata::TRB3RawDigit>&);
-      std::array<double, n_seg>    GetTdc(const std::vector<rawdata::TRB3RawDigit>&);
-      
-      emph::cmap::ChannelMap* fChannelMap;
-      std::string fChanMapFileName;
-      unsigned int fRun;
-      unsigned int fSubrun;
-      unsigned int fNEvents;
-      
-      TTree *tree;
-      std::array<float, n_ch_det> ADCq;
-      std::array<float, n_ch_det> ADCmax;
-      std::array<float, n_ch_det> ADCblw;
+    bool   FindSeg(int);
+    bool   FindTrailBot(int);
+    bool   FindTrailTop(int);
+    bool   FindLeadBot(int);
+    bool   FindLeadTop(int);
+    int    GetSegIdTrb3(int);
+    std::array<int, 2> GetSegChTrb3(int);
+    void   FillT0AnaTree(art::Handle< std::vector<rawdata::WaveForm> > &,
+			 art::Handle< std::vector<rawdata::TRB3RawDigit> > &);
+    std::array<double, N_CH_DET> GetTot(const std::vector<rawdata::TRB3RawDigit>&);
+    std::array<double, N_SEG>    GetTdc(const std::vector<rawdata::TRB3RawDigit>&);
 
-      std::array<double, n_seg> TDCt;
-      std::array<double, n_ch_det> TDCtot;
-    };
+    emph::cmap::ChannelMap* fChannelMap;
+    std::string fChanMapFileName;
+    unsigned int fRun;
+    unsigned int fSubrun;
+    unsigned int fNEvents;
+
+    std::array<double, n_ch> time_ch;
+    std::array<bool, n_ch> found_ch;
+    std::array<double, n_ch_det> T0_tot;
+    std::array<double, n_seg> T0_tdc;
+
+    TTree *tree;
+    std::array<double, n_ch_det> ADCq;
+    std::array<double, n_ch_det> ADCmax;
+    std::array<double, n_ch_det> ADCblw;
+
+    std::array<double, n_seg> TDCt;
+    std::array<double, n_ch_det> TDCtot;
+
+  };
 
   //.......................................................................
   T0Ana::T0Ana(fhicl::ParameterSet const& pset)
     : EDAnalyzer(pset)
   {
-    
+
     this->reconfigure(pset);
 
     // create TTree for TRB3RawDigits
@@ -106,7 +115,7 @@ namespace emph {
     tree->Branch("qmax",&ADCmax);
     tree->Branch("qblw",&ADCblw);
 
-    tree->Branch("t", &TDCq);
+    tree->Branch("t", &TDCt);
     tree->Branch("ttot", &TDCtot);
   }
 
@@ -117,7 +126,7 @@ namespace emph {
     // Clean up any memory allocated by your module
     //======================================================================
   }
-  
+
   //......................................................................
   void T0Ana::reconfigure(const fhicl::ParameterSet& pset)
   {
@@ -140,22 +149,20 @@ namespace emph {
       std::cout << "Loaded channel map from file " << fChanMapFileName << std::endl;
     }
   }
-  
+
   //......................................................................
   void T0Ana::endJob()
   {
-    
-  }
-  
-  //......................................................................
 
+  }
+
+  //......................................................................
   bool T0Ana::FindSeg(int ch_daq)
   {
     if (ch_daq >= 1 && ch_daq <= 40) return true;
 
     return false;
   }
-
 
   //......................................................................
   int T0Ana::GetSegIdTrb3(int ch_daq)
@@ -172,7 +179,6 @@ namespace emph {
     if(ch_daq == 19 || ch_daq == 20 || ch_daq == 39 || ch_daq == 40) return 9;
     return -1;
   }
-
 
   //......................................................................
   std::array<int, 2> T0Ana::GetSegChTrb3(int seg_id)
@@ -200,7 +206,6 @@ namespace emph {
     }
   }
 
-
   //......................................................................
   bool T0Ana::FindTrailBot(int ch_daq)
   {    
@@ -221,30 +226,26 @@ namespace emph {
     return false;
   }
   //......................................................................
-  int T0Ana::FindLeadTop(int ch_daq)
+  bool T0Ana::FindLeadTop(int ch_daq)
   {
     if ((ch_daq > 20) && ((ch_daq%2)==1)) return true;
     return false;
   }
 
   //......................................................................
-  std::array<double, n_ch_det> T0Ana::GetTot(const std::vector<rawdata::TRB3RawDigit>& digvec)
+  std::array<double, N_CH_DET> T0Ana::GetTot(const std::vector<rawdata::TRB3RawDigit>& digvec)
   {
-    std::array<double, n_ch> time_ch;
-    std::array<bool, n_ch> found_ch;
-    std::array<double, n_ch_det> T0_tot;
-
-    for(int i_ch; i_ch < n_ch; i_ch++){
+    for(int i_ch = 0; i_ch < n_ch; i_ch++){
       time_ch[i_ch]  = 0;
       found_ch[i_ch] = false;
     }
 
-    for(int i_ch_det; i_ch_det < n_ch_det; i_ch_det++){
+    for(int i_ch_det = 0; i_ch_det < n_ch_det; i_ch_det++){
       T0_tot[i_ch_det]  = -999.0;
     }
 
     //loop over TRB3 signals
-    int n_vec = digvec->size();
+    int n_vec = digvec.size();
     for(int i_vec = 0; i_vec < n_vec; i_vec++){
       uint32_t evt_ch = digvec.at(i_vec).GetChannel();
 
@@ -252,9 +253,8 @@ namespace emph {
 	time_ch[evt_ch] = epoch_const*digvec.at(i_vec).GetEpochCounter()
 	                   + coarse_const*digvec.at(i_vec).GetCoarseTime()
 	                   + coarse_const*(digvec.at(i_vec).GetFineTime() - trb3_linear_low)/(trb3_linear_high_T0 - trb3_linear_low);
-	found_ch[eve_ch] = true;
+	found_ch[evt_ch] = true;
       }
-
     }//end loop over T0 TRB3 signals
 
     //Calculation of TOT, loop over T0 leading channels
@@ -268,23 +268,19 @@ namespace emph {
   }
 
   //......................................................................
-  std::array<double, n_seg> T0Ana::GetTdc(const std::vector<rawdata::TRB3RawDigit>& digvec)
+  std::array<double, N_SEG> T0Ana::GetTdc(const std::vector<rawdata::TRB3RawDigit>& digvec)
   {
-    std::array<double, n_ch> time_ch;
-    std::array<bool, n_ch> found_ch;
-    std::array<double, n_seg> T0_tdc;
-
-    for(int i_ch; i_ch < n_ch; i_ch++){
+    for(int i_ch = 0; i_ch < n_ch; i_ch++){
       time_ch[i_ch]  = 0;
       found_ch[i_ch] = false;
     }
 
-    for(int i_ch_det; i_ch_det < n_ch_det; i_ch_det++){
+    for(int i_ch_det = 0; i_ch_det < n_ch_det; i_ch_det++){
       T0_tdc[i_ch_det]  = -100000000.0;
     }
 
     //loop over TRB3 signals
-    int n_vec = digvec->size();
+    int n_vec = digvec.size();
     for(int i_vec = 0; i_vec < n_vec; i_vec++){
       uint32_t evt_ch = digvec.at(i_vec).GetChannel();
 
@@ -292,12 +288,12 @@ namespace emph {
 	time_ch[evt_ch] = epoch_const*digvec.at(i_vec).GetEpochCounter()
 	                   + coarse_const*digvec.at(i_vec).GetCoarseTime()
 	                   + coarse_const*(digvec.at(i_vec).GetFineTime() - trb3_linear_low)/(trb3_linear_high_T0 - trb3_linear_low);
-	found_ch[eve_ch] = true;
+	found_ch[evt_ch] = true;
       }else if(FindSeg(evt_ch) && (!found_ch[evt_ch])){
 	time_ch[evt_ch] = epoch_const*digvec.at(i_vec).GetEpochCounter()
 	                   + coarse_const*digvec.at(i_vec).GetCoarseTime()
 	                   + coarse_const*(digvec.at(i_vec).GetFineTime() - trb3_linear_low)/(trb3_linear_high_T0 - trb3_linear_low);
-	found_ch[eve_ch] = true;
+	found_ch[evt_ch] = true;
       }
     }//end loop over T0 TRB3 signals
 
@@ -315,110 +311,99 @@ namespace emph {
   }
 
   //......................................................................
-  
+
   void T0Ana::FillT0AnaTree(art::Handle< std::vector<emph::rawdata::WaveForm> > & T0wvfm, art::Handle< std::vector<emph::rawdata::TRB3RawDigit> > & T0trb3)
-    {
-      for (int i=0; i<20; ++i) {
-	ADCq[i] = -999999.;
-	ADCmax[i] = -9999.;
-	ADCblw[i] = -1.;	
+  {
+    for (int i=0; i<20; ++i) {
+      ADCq[i] = -999999.0;
+      ADCmax[i] = -9999.0;
+      ADCblw[i] = -1.0;
 
-	TDCt[i] = -100000000.0;
-	TDCtot[i] = -999.;
-      }
-      
-      // get ADC info for T0
-      if (!T0wvfm->empty()) {
-	emph::cmap::FEBoardType boardType = emph::cmap::V1720;
-	emph::cmap::EChannel T0echan;
-	T0echan.SetBoardType(boardType);
-	// loop over ADC channels
-	for (size_t idx=0; idx < T0wvfm->size(); ++idx) { 
-	  const rawdata::WaveForm& wvfm = (*T0wvfm)[idx];
-	  int chan = wvfm.Channel();
-	  int board = wvfm.Board();
-	  echan.SetBoard(board);
-	  echan.SetChannel(chan);
-	  emph::cmap::DChannel dchan = fChannelMap->DetChan(echan);
-	  int detchan = dchan.Channel();
-	  if (detchan >0 && detchan <= 20){
-	    ADCq[detchan - 1] = wvfm.Charge(); 
-	    ADCmax[detchan - 1] = wvfm.Baseline()-wvfm.PeakADC(); 
-	    ADCblw[detchan - 1] = wvfm.BLWidth();
-	  }	  
-	} // end loop over T0 ADC channels
-      }
-
-      // get TDC info for T0
-      if (!T0trb3->empty()) {
-	TDCt = GetTdc(T0trb3);
-	TDCtot = GetTot(T0trb3);
-
-	emph::cmap::FEBoardType boardType = emph::cmap::TRB3;
-	emph::cmap::EChannel T0echan;
-	T0echan.SetBoardType(boardType);
-	// loop over ADC channels
-	for (size_t idx=0; idx < T0trb3->size(); ++idx) { 
-	  const rawdata::TRB3RawDigit& trb3 = (*T0trb3)[idx];
-	  int chan = trb3.GetChannel();
-	  int board = trb3.GetBoardId();
-	  echan.SetBoard(board);
-	  echan.SetChannel(chan);
-	  emph::cmap::DChannel dchan = fChannelMap->DetChan(echan);
-	  int detchan = dchan.Channel();
-	  if (detchan >0 && detchan <= 20){
-	    Tq[detchan] = wvfm.Charge(); 
-	    Tmax[detchan] = wvfm.Baseline()-wvfm.PeakADC(); 
-	    Tblw[detchan] = wvfm.BLWidth();
-	  }	  
-	} // end loop over T0 ADC channels
-      }
-      tree->Fill();
+      TDCt[i] = -100000000.0;
+      TDCtot[i] = -999.0;
     }
-    
-    //......................................................................
 
-    void T0Ana::analyze(const art::Event& evt)
-    { 
-      ++fNEvents;
-      fRun = evt.run();
-      fSubrun = evt.subRun();     
-      std::string labelStrBAC, labelStrT0, labelStrGC, labelStrLG;
-
-      // get WaveForm
-      int i = emph::geo::BACkov;
-      labelStrBAC = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(i));
-      art::Handle< std::vector<emph::rawdata::WaveForm> > wfHandleBAC;
-      
-      int j = emph::geo::T0;
-      labelStrT0 = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(j));
-      art::Handle< std::vector<emph::rawdata::WaveForm> > wfHandleT0;
-
-      int k = emph::geo::GasCkov;
-      labelStrGC = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(k));
-      art::Handle< std::vector<emph::rawdata::WaveForm> > wfHandleGC;
-
-      int l = emph::geo::LGCalo;
-      labelStrLG = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(l));
-      art::Handle< std::vector<emph::rawdata::WaveForm> > wfHandleLG;
-
-
-      try {
-	evt.getByLabel(labelStrBAC, wfHandleBAC);
-	evt.getByLabel(labelStrT0, wfHandleT0);
-	evt.getByLabel(labelStrGC, wfHandleGC);
-	evt.getByLabel(labelStrLG, wfHandleLG);
-
-	if (!wfHandleBAC->empty()) {
-	  FillBACkovPlots(wfHandleBAC, wfHandleT0, wfHandleGC, wfHandleLG);
+    // get ADC info for T0
+    if (!T0wvfm->empty()) {
+      emph::cmap::FEBoardType boardType = emph::cmap::V1720;
+      emph::cmap::EChannel T0echan;
+      T0echan.SetBoardType(boardType);
+      // loop over ADC channels
+      for (size_t idx=0; idx < T0wvfm->size(); ++idx){
+	const rawdata::WaveForm& wvfm = (*T0wvfm)[idx];
+	int chan = wvfm.Channel();
+	int board = wvfm.Board();
+	T0echan.SetBoard(board);
+	T0echan.SetChannel(chan);
+	emph::cmap::DChannel dchan = fChannelMap->DetChan(T0echan);
+	int detchan = dchan.Channel();
+	if (detchan > 0 && detchan <= 20){
+	  ADCq[detchan - 1] = wvfm.Charge();
+	  ADCmax[detchan - 1] = wvfm.Baseline()-wvfm.PeakADC();
+	  ADCblw[detchan - 1] = wvfm.BLWidth();
 	}
-      }
-      catch(...) {
-
-      }
-
-      return;
+      } // end loop over T0 ADC channels
     }
+
+    // get TDC info for T0
+    if (!T0trb3->empty()) {
+      // TDCt = GetTdc(T0trb3);
+      // TDCtot = GetTot(T0trb3);
+
+      // emph::cmap::FEBoardType boardType = emph::cmap::TRB3;
+      // emph::cmap::EChannel T0echan;
+      // T0echan.SetBoardType(boardType);
+      // // loop over ADC channels
+      // for (size_t idx=0; idx < T0trb3->size(); ++idx){
+      //   const rawdata::TRB3RawDigit& trb3 = (*T0trb3)[idx];
+      //   int chan = trb3.GetChannel();
+      //   int board = trb3.GetBoardId();
+      //   echan.SetBoard(board);
+      //   echan.SetChannel(chan);
+      //   emph::cmap::DChannel dchan = fChannelMap->DetChan(echan);
+      //   int detchan = dchan.Channel();
+      //   if (detchan >0 && detchan <= 20){
+      //     Tq[detchan] = wvfm.Charge();
+      //     Tmax[detchan] = wvfm.Baseline()-wvfm.PeakADC();
+      //     Tblw[detchan] = wvfm.BLWidth();
+      //   }
+      // } // end loop over T0 ADC channels
+    }
+    tree->Fill();
+  }
+
+  //......................................................................
+  void T0Ana::analyze(const art::Event& evt)
+  {
+    ++fNEvents;
+    fRun = evt.run();
+    fSubrun = evt.subRun();
+    std::string labelStrT0, labelStrRPC;
+
+    // get WaveForm
+    int i = emph::geo::T0;
+    labelStrT0 = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(i));
+    art::Handle< std::vector<emph::rawdata::TRB3RawDigit> > HandleT0trb3;
+    art::Handle< std::vector<emph::rawdata::WaveForm> > HandleT0wvfm;
+
+    int j = emph::geo::RPC;
+    labelStrRPC = "raw:" + emph::geo::DetInfo::Name(emph::geo::DetectorType(j));
+    art::Handle< std::vector<emph::rawdata::TRB3RawDigit> > HandleRPCtrb3;
+
+    try {
+      evt.getByLabel(labelStrT0, HandleT0trb3);
+      evt.getByLabel(labelStrT0, HandleT0wvfm);
+      evt.getByLabel(labelStrRPC, HandleRPCtrb3);
+
+      if (!HandleT0trb3->empty()) {
+	FillT0AnaTree(HandleT0wvfm, HandleT0trb3);
+      }
+    }
+    catch(...) {
+
+    }
+
+    return;
   }
 } // end namespace demo
 
