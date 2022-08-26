3b1ecd14b469136448d21732ddc27d2fd50d5673
Jonathan Paley <jpaley@fnal.gov>
2022-08-26 17:15:57 -0500

    Add gas ckov pressures to spill class.  Removed momentum information for now, getting non-sensible results from ifbeam.  Everything else looks reasonable.

--- SpillInfo/SpillInfo_module.cc
+++ SpillInfo/SpillInfo_module.cc
@@ -11,24 +11,10 @@
 #include "art/Framework/Core/ModuleMacros.h"
 
 ////////////////////////////////////////////////////////////////////////
-/// \brief   This module reads IFDB and then stores MCenter spill info
-/// \author lackey32@fnal.gov
+/// \brief   This module reads IFDB and then stores MTest spill info.
+///          Code based on NOvA Testbeam
+/// \author jpaley@fnal.gov, lackey32@fnal.gov
 ////////////////////////////////////////////////////////////////////////
-
-//C++ includes 
-#include <iostream>
-#include <fstream>
-#include <math.h>
-#include <string>
-#include <time.h> 
-#include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
 
 // Framework includes
 #include "art/Framework/Core/EDProducer.h"
@@ -61,7 +47,6 @@ namespace emph
     void endSubRun(art::SubRun &sr);
 
   private:
-    bool fNeedNewSpillInfo;
 
     std::unique_ptr<rb::Spill> fSpill;
     double fOffset;
@@ -82,11 +67,9 @@ namespace emph
     fOffset         (pset.get< double      >("Offset")         ),
     bfp( ifbeam_handle->getBeamFolder(pset.get< std::string >("Bundle"), pset.get< std::string >("URL"), pset.get< double >("TimeWindow")))
   {
-    fNeedNewSpillInfo = true;
 
-    // how close in time does the spill time have to be from the DAQ tims [seconds]
-    // FTBF has one 4.2 second spill every minute.
-    bfp->set_epsilon(5); 
+    // FTBF has one 4.2 second spill every minute.  Get the information from the nearest entry within a minute.
+    bfp->set_epsilon(60); 
     // tell the module what it is making as this is a EDProducer
     produces< rb::Spill, art::InSubRun >();
     // Be noisy to demonstrate what's happening
@@ -101,55 +84,81 @@ namespace emph
   }
   
   //......................................................................
-  void SpillInfo::beginSubRun(art::SubRun& )//sr)
+  void SpillInfo::beginSubRun(art::SubRun& sr)
   {
-    fNeedNewSpillInfo = true;
+    std::cout << "%%%%% Subrun time: " << sr.beginTime().timeHigh() << std::endl;
+    fSpill.reset(new rb::Spill());
+    fSpill->SetTimestamp(sr.beginTime().timeHigh());
+    
+    // Query the database to get post-spill sample device info. 
+    // We are querying $36, so an offset is added to the event time 
+    // to match the db time. Epsilon for the beam folder is large enough
+    // to encompass full spill
+    try {
+      double spillTime = (double)sr.beginTime().timeHigh();
+      double recordTime = spillTime + fOffset;
+      
+      double intensity;
+      //      double momentum;
+      double mt5cpr;
+      double mt6cpr;
+      double mt5cp2;
+      double mt6cp2;
+      double tbccdi;
+      double tbccdo;
+      double mt6ca1;
+      double mt6ca2;
+      double mt6ca3;
+      
+      // JMP: the beam momentum recording appears to be broken.  Not sure
+      // right now what to do about this.  Skipping momentum info for now.
+      //      bfp->GetNamedData(recordTime,"S:MTNRG",&momentum);
 
+
+      bfp->GetNamedData(recordTime,"F:MT6SC1",&intensity);
+      bfp->GetNamedData(recordTime,"F:MT5CPR,F:MT6CPR,F:MT5CP2,F:MT6CP2",&mt5cpr,&mt6cpr,&mt5cp2,&mt6cp2);
+      bfp->GetNamedData(recordTime,"F:TBCCDI,F:TBCCDO",&tbccdi,&tbccdo);
+      bfp->GetNamedData(recordTime,"F:MT6CA1,F:MT6CA2,F:MT6CA3",&mt6ca1,&mt6ca2,&mt6ca3);
+      
+      fSpill->SetIntensity(intensity);
+      //      fSpill->SetMomentum(momentum);
+      fSpill->SetMT5CPR(mt5cpr);
+      fSpill->SetMT6CPR(mt6cpr);
+      fSpill->SetMT5CP2(mt5cp2);
+      fSpill->SetMT6CP2(mt6cp2);
+      fSpill->SetTBCCDI(tbccdi);
+      fSpill->SetTBCCDO(tbccdo);
+      fSpill->SetMT6CA1(mt6ca1);
+      fSpill->SetMT6CA2(mt6ca2);
+      fSpill->SetMT6CA3(mt6ca3);
+
+      std::cout << "%%%%% IFBeam query results: %%%%%" << std::endl;
+      std::cout << "Time: spillTime+fOffset = " << uint64_t(spillTime+fOffset) << std::endl;
+      std::cout << "Intensity: " << intensity << std::endl;
+      //      std::cout << "Momentum: " << momentum << std::endl;
+      std::cout << "CkovMT5 Pressure: " << mt5cpr << std::endl;
+      std::cout << "CkovMT6 Pressure: " << mt6cpr << std::endl;
+
+    }
+    catch (const WebAPIException &we) {
+      std::string err(we.what());
+      if (!err.find("variable not found")) {
+	mf::LogError("SpillInfo") << "Exception encountered: " 
+				  << we.what();
+	std::abort();
+      }
+    }  
   }
-
+  
   //......................................................................
   void SpillInfo::endSubRun(art::SubRun& sr)
   {
     sr.put(std::move(fSpill));
-    fNeedNewSpillInfo = true;
   }
 
   //......................................................................
-  void SpillInfo::produce(art::Event& evt)
+  void SpillInfo::produce(art::Event& ) //evt)
   {
-    
-    if(!evt.isRealData()){
-      // don't have any beam sim to put in here      
-      return;
-    }
-    if (fNeedNewSpillInfo) {
-      fSpill.reset(new rb::Spill());
-      fSpill->SetTimestamp(evt.time().timeHigh());
-
-      // Query the database to get post-spill sample device info. 
-      // We are querying $36, so an offset is added to the event time 
-      // to match the db time. Epsilon for the beam folder is large enough
-      // to encompass full spill
-      try {
-	double spillTime = (double)evt.time().timeHigh();
-
-	float intensity;
-	float momentum;
-
-	bfp->GetNamedData(spillTime+fOffset,"F:MT6SC1,S:MTNRG", &intensity,&momentum);
-	fSpill->SetIntensity(intensity);
-	fSpill->SetMomentum(momentum);
-	fNeedNewSpillInfo = false;
-      }
-      catch (const WebAPIException &we) {
-	std::string err(we.what());
-	if (!err.find("variable not found")) {
-	  mf::LogError("SpillInfo") << "Exception encountered: " 
-				    << we.what();
-	  std::abort();
-	}
-      }      
-    }
     
   }
   
