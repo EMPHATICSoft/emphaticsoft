e549ecb92a53be7ae4b4d94c51a43d3827730347
Jonathan Paley <jpaley@fnal.gov>
2022-08-29 22:41:20 -0500

    Fix CMake file for SpillInfo (not sure why this ever compiled in my branch tobegin with...).  Adding SSDCluster (collection of SSDHits in the same view) class to RecoBase.

--- RecoBase/classes.h
+++ RecoBase/classes.h
@@ -1,5 +1,6 @@
 #include "canvas/Persistency/Common/Wrapper.h"
 #include "RecoBase/SSDHit.h"
+#include "RecoBase/SSDCluster.h"
 #include "RecoBase/TrackSegment.h"
 #include "RecoBase/SpacePoint.h"
 #include "RecoBase/Spill.h"
