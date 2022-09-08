3dfd086c01b0ebe1a950a3e1509ed66e5a001e60
Gavin Davies <gsdavies@fnal.gov>
2022-09-08 06:32:37 -0500

    missing include for Run

--- ARICHReco/ARICHReco_module.cc
+++ ARICHReco/ARICHReco_module.cc
@@ -19,6 +19,7 @@
 #include "art/Framework/Core/EDProducer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Run.h"
 #include "art/Framework/Principal/Handle.h"
 #include "art_root_io/TFileService.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
