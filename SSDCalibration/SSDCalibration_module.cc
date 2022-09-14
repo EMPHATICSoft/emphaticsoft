07cb0b3d5dec40929a5f9032c7df28b2d6583f8f
Linyan Wan <lwan@emphaticgpvm01.fnal.gov>
2022-09-14 15:58:47 -0500

    Calibration module

---
+++ SSDCalibration/SSDCalibration_module.cc
@@ -0,0 +1,153 @@
+////////////////////////////////////////////////////////////////////////
+/// \brief   Producer module to create calibration constants for SSD
+///          alignment
+/// \author  $Author: linyan $
+////////////////////////////////////////////////////////////////////////
+// C/C++ includes
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+// ROOT includes
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH2F.h"
+
+// Framework includes
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art_root_io/TFileService.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "canvas/Persistency/Common/Ptr.h"
+#include "canvas/Persistency/Common/PtrVector.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+// EMPHATICSoft includes
+#include "ChannelMap/ChannelMap.h"
+#include "RunHistory/RunHistory.h"
+#include "Geometry/DetectorDefs.h"
+#include "RawData/TRB3RawDigit.h"
+
+using namespace emph;
+
+///package to illustrate how to write modules
+namespace emph {
+  
+  ///
+  class SSDCalibration : public art::EDProducer {
+  public:
+    explicit SSDCalibration(fhicl::ParameterSet const& pset); // Required! explicit tag tells the compiler this is not a copy constructor
+    ~SSDCalibration();
+    
+    // Optional, read/write access to event
+    void produce(art::Event& evt);
+    
+    // Optional if you want to be able to configure from event display, for example
+    void reconfigure(const fhicl::ParameterSet& pset);
+    
+    // Optional use if you have histograms, ntuples, etc you want around for every event
+    void beginRun(art::Run& run);
+    void endRun(art::Run const&);
+    //      void endSubRun(art::SubRun const&);
+    void endJob();
+    void Fit();
+    
+  private:
+    
+    emph::cmap::ChannelMap* fChannelMap;
+	 runhist::RunHistory* fRunHistory;
+    int         fEvtNum;
+	 std::vector<rb::SSDHit> ssdvec;
+
+  };
+
+  //.......................................................................
+  
+  SSDCalibration::SSDCalibration(fhicl::ParameterSet const& pset)
+    : EDProducer(pset)
+  {
+
+//    this->produces<std::vector<rb::ARing>>();
+
+    //this->reconfigure(pset);
+    fEvtNum = 0;
+
+  }
+
+  //......................................................................
+  
+  SSDCalibration::~SSDCalibration()
+  {
+    //======================================================================
+    // Clean up any memory allocated by your module
+    //======================================================================
+  }
+
+  //......................................................................
+
+  // void SSDCalibration::reconfigure(const fhicl::ParameterSet& pset)
+  // {    
+  // }
+
+  //......................................................................
+  
+  void SSDCalibration::beginRun(art::Run& run)
+  {
+    // initialize channel map
+    fChannelMap = new emph::cmap::ChannelMap();
+	 fRunHistory = new runhist::RunHistory(run.run());
+    fChannelMap->LoadMap(fRunHistory->ChanFile());
+
+  }
+    
+  //......................................................................
+ 
+  void SSDCalibration::endRun()
+  {
+	  Fit();
+  }
+  
+  //......................................................................
+  
+  void SSDCalibration::endJob()
+  {
+	  //Output the alignment constants to ConstBase/SSDCalibration.dat
+  }
+  
+  //......................................................................
+  
+  void SSDCalibration::Fit()
+  {
+   //Fit the ssdvectors and obtain x, y, z shifts
+  }
+  
+  //......................................................................
+  void SSDCalibration::produce(art::Event& evt)
+  { 
+	  //SSDRawDigit to SSDHit
+    std::string labelstr = "raw:SSDRawDigit";
+    // get arich trb3digits
+    std::unique_ptr<std::vector<rb::ARing> > aringv(new std::vector<rb::ARing>);
+    
+    art::Handle< std::vector<emph::rawdata::TRB3RawDigit> > trbhandle;
+    try {
+      evt.getByLabel(labelstr, trbhandle);
+      if (!trbhandle->empty()) {	
+	GetARings(trbhandle,aringv);
+	fEvtNum++;
+      }
+    }
+    catch(...) {
+      
+    }
+    evt.put(std::move(aringv));
+
+  }
+
+  } // end namespace emph
+
+DEFINE_ART_MODULE(emph::SSDCalibration)
