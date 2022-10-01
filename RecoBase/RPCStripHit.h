df723fa081dae5df1fdd0c326162a25663b530da
Paul L. G. Lebrun <lebrun@fnal.gov>
2022-10-01 06:58:43 -0500

    add access time over threshold data

--- RecoBase/RPCStripHit.h
+++ RecoBase/RPCStripHit.h
@@ -12,7 +12,7 @@ namespace rb {
   class RPCStripHit {
   public:
     RPCStripHit(); // Default constructor
-    RPCStripHit(int StripNumber, double tLeft, double tRight, const emph::geo::Detector &st); // Default constructor
+    RPCStripHit(int StripNumber, double tLeft, double tRight, const emph::geo::Detector &st); // specific constructor.  Future version. 
     virtual ~RPCStripHit() {}; //Destructor
     
   private:
@@ -20,14 +20,16 @@ namespace rb {
     int _stripNum; // rotation angle about the vertical y-axis
     double _pitch;
 
-    double _x, _y, _z, _tLeft, _tRight;
+    double _x, _y, _z, _tLeft, _tRight, _totLeft, _totRight;
     double _tCal;
 
   public:
     // Getters
     inline double StripNumber() const { return _stripNum; }
-    inline double Time() const { return 0.5 * (_tLeft + _tRight); }
+    inline double Time() const { return 0.5 * (_tLeft + _tRight); } // To be reviewed when we have the SSD tracks.. 
     inline double TimeLeft() const { return _tLeft; }
+    inline double TimeOverThresholdLeft() const { return _totLeft; }
+    inline double TimeOverThresholdRight() const { return _totRight; }
     inline double TimeRight() const { return _tRight; }
     inline double TimeCal() const { return _tCal; }
 
@@ -35,6 +37,7 @@ namespace rb {
     inline void SetStripNumber(int n) { _stripNum = n; }
     inline void SetPitch(double pitch) { _pitch = pitch; }
     inline void SetTimeLeftRight(double tL, double tR) { _tLeft = tL, _tRight = tR; }
+    inline void SetTimeOverThresholdLeftRight(double tL, double tR) { _totLeft = tL, _totRight = tR; }
 
     void CalibrateTime(double *calLeft, double *calRight); 
     void CalibratePosition(double *xShit, double *yShift, double *zShift); // to be implemented, 
