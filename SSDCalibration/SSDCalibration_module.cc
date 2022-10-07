ac87f9feea606f817938b9b29b425360085cad40
Linyan Wan <lwan@emphaticgpvm01.fnal.gov>
2022-10-07 09:43:55 -0500

    temporary solution to fetch SSD info

--- SSDCalibration/SSDCalibration_module.cc
+++ SSDCalibration/SSDCalibration_module.cc
@@ -140,9 +140,12 @@ namespace emph {
 				  emph::cmap::DChannel dchan = fChannelMap->DetChan(echan);
 				  const emph::geo::SSDStation &st = emgeo->GetSSDStation(dchan.Station());
 				  const emph::geo::Detector &sd = st.GetSSD(dchan.Channel());
-				  double x =
 				  rb::SSDHit hit(ssd, sd);
 				  ssdvec.push_back(hit);
+				  double x = (ssd.Row()*hit.Pitch()-sd.Height()/2)*sin(sd.Rot())+sd.Pos()[0];
+				  double y = (ssd.Row()*hit.Pitch()-sd.Height()/2)*cos(sd.Rot())+sd.Pos()[1];
+				  double z = st.Pos()[2] + sd.Pos()[2];
+				  if ( fEvtNum < 100 ) std::cout << x << " " << y << " " << z << std::endl;
 			  }
 			  fEvtNum++;
 		  }
