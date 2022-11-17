577c57bbf2405f39ff2424f60fda39b58b8d4d5c
Gavin Davies <gsdavies@fnal.gov>
2022-11-16 21:14:36 -0600

    move Demo to demo (consistent lowercase sub-directories). noticed because the original Demo wasn't added as a sub-dir to CMakeLists. Also need art::module in SSDReco cmake.

---
+++ Geometry/demo/test.cxx
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////
+/// \brief   unit test for the geometry package
+/// \author  wanly@bu.edu
+/// \date
+////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+
+#include "Geometry/Geometry.h"
+#include "Geometry/DetectorDefs.h"
+#include "RunHistory/RunHistory.h"
+
+using namespace emph::geo;
+
+int main(int argc, char* argv[]){
+
+	if(argc !=2){
+		std::cerr << "Usage: UnitTest [Run-number]" <<std::endl;
+		return -1;
+	}
+	
+	int runNum = std::stoi(argv[1]);
+	std::cout << "This is a unit test for Run" << runNum << "." << std::endl << std::endl;
+
+	runhist::RunHistory *fRunHistory = new runhist::RunHistory(runNum);
+	Geometry *emgeo = new Geometry(fRunHistory->GeoFile());
+
+	std::cout << "The magnet position is " << emgeo->MagnetUSZPos() << " - " << emgeo->MagnetDSZPos() << " mm." << std::endl;
+
+	for ( int i = Trigger ; i < NDetectors ; i++ ){
+		if ( !emgeo->DetectorLoad(i) )continue;
+		std::cout << "The " << DetInfo::Name(DetectorType(i)) << " position is " << emgeo->DetectorUSZPos(i) << " - " << emgeo->DetectorDSZPos(i) << " mm." << std::endl;
+	}
+
+	int nstation = emgeo->NSSDStations();
+	for ( int i = 0; i < nstation; i++){
+		SSDStation st = emgeo->GetSSDStation(i);
+		int nsensor = st.NSSDs();
+		for ( int j = 0; j < nsensor; j++){
+			Detector sensor = st.GetSSD(j);
+			std::cout << "The " << j <<"-th SSD sensor in the " << i <<"-th SSD station is located at " << sensor.Pos()[0] << " " << sensor.Pos()[1] << " " << sensor.Pos()[2]+st.Pos()[2] << " mm." << std::endl;
+			std::cout << "The rotation angle is " << sensor.Rot() << std::endl;
+		}
+	}
+
+	std::cout << std::endl << "This unit test is finished." << std::endl << std::endl;
+
+	return 1;
+
+}
