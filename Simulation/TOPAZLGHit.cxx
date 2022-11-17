2a13b56c82bd6e65677bca3ee55ad894f4c0fa74
Paul L. G. Lebrun <lebrun@fnal.gov>
2022-11-17 13:27:54 -0600

    Simulated LGCalo hits

---
+++ Simulation/TOPAZLGHit.cxx
@@ -0,0 +1,85 @@
+////////////////////////////////////////////////////////////////////////
+/// \brief   Implemantation of TOPAZLGHit
+/// \author  lebrun@fnal.gov
+/// \date
+////////////////////////////////////////////////////////////////////////
+
+#include <climits>
+#include <cfloat>
+#include <iostream>
+#include <fstream>
+
+#include "Simulation/TOPAZLGHit.h"
+
+namespace sim {
+
+  TOPAZLGHit::TOPAZLGHit() :
+  fBlock(INT_MAX), // from 0 to 9
+  fEDeposited(0.), // in GeV, or some pre-calibrated random units. 
+  fX(3, DBL_MAX), // dimensioned to 3. Compute averages in FinalizeAndCalibrate  
+  fNPhots(0),
+  fNunSteps(0),  
+  fWaveForm(50, 0.) // 50 4 ns bin, too much for one promt shwer, but O.K.  Perhaps we ought simulate multi-bucket, or 
+                                      // unexpectdely slow Cerenkohv ( Even perhaps Dark Cerenkov radiation + break down of Lorentz Invariance) 
+//  fTrackAmpls(),
+//  fItTrackLastUsed(fTrackAmpls.rbegin())
+  {
+
+  }
+  void TOPAZLGHit::Reset() {
+    fBlock = INT_MAX;
+    fEDeposited = 0.;
+    for (size_t k=0; k != fX.size(); k++) fX[k] = 0.;
+    fNPhots = 0; fNunSteps=0;
+    for (size_t k=0; k != fWaveForm.size(); k++) fWaveForm[k] = 0.;
+//    fTrackAmpls.clear(); fItTrackLastUsed = fTrackAmpls.rbegin();
+  }
+  /*
+  int TOPAZLGHit::GetAncestorTrackNum() const { // Take the one with the highest photon count. 
+    if (fTrackAmpls.size() == 0) return 0;
+    std::vector<std::pair<int, int> >::const_iterator itSel = fTrackAmpls.cbegin();
+    int nPhotSel=0;
+    for (std::vector<std::pair<int, int> >::const_iterator it = fTrackAmpls.cbegin(); it != fTrackAmpls.cend(); it++) {
+      if (it->second > nPhotSel) { nPhotSel = it->second; itSel = it; } 
+    }
+    return itSel->first;
+  }
+  void TOPAZLGHit::AddAncestorTrack(int ancestorTrackNum) {
+  
+    for (std::vector<std::pair<int, int> >::iterator it = fTrackAmpls.begin(); it != fTrackAmpls.end(); it++) {
+      if (it->first == ancestorTrackNum) { // Already got it.. 
+         for (std::vector<std::pair<int, int> >::reverse_iterator itR = fTrackAmpls.rbegin(); itR != fTrackAmpls.rend(); itR++) {
+	   if (itR->first == ancestorTrackNum)  { fItTrackLastUsed = itR; return; } 
+	 } // double loop.. Hopefully, not many of them.  For Energy resolution studies, only one, the primary electron.
+      }
+    } 
+    std::pair<int, int>  aTr; aTr.first = ancestorTrackNum; aTr.second = 0;
+    fTrackAmpls.push_back(aTr);
+    fItTrackLastUsed = fTrackAmpls.rbegin();
+  }
+  */
+  void TOPAZLGHit::AddSomePhotons(int numPhot, double x, double y, double z, double t) { // To the current track ! 
+    size_t itTime = 4.0*t; // should add here noise due to the V1720 time digitizer. Assume 250 MHz. 
+    if (itTime >= fWaveForm.size()) return; // Out of time condition.. 
+    fNPhots += numPhot;
+    fNunSteps++;
+    fX[0] += x*numPhot;
+    fX[1] += y*numPhot;
+    fX[2] += z*numPhot;
+    fWaveForm[itTime] += numPhot;
+//    if ((fTrackAmpls.size() == 0) || (fItTrackLastUsed == fTrackAmpls.rbegin())) {
+//      std::cerr << " TOPAZLGHit::AddSomePhotons error, can not add photon to an unknow track.  Use Add AddTrackAncestorTrack first.. Fatal " << std::endl;
+//      exit(2);
+//    }
+//    std::vector<<std::pair<int, int> >::reverse_iterator itR =  fTrackAmpls.rbegin();
+//    fItTrackLastUsed->second += numPhot;
+  
+  }  // to called by the user action upon a new G4Step, after setting the Ancestor track number  
+  void TOPAZLGHit::FinalizeAndCalibrate(double cFact) {
+  
+    for (size_t k=0; k != 3; k++) fX[k] /= fNPhots;
+    fEDeposited = fNPhots * cFact;
+    
+  }
+}
+
---
+++ Simulation/TOPAZLGHit.h
@@ -0,0 +1,76 @@
+////////////////////////////////////////////////////////////////////////
+/// \brief   A V1720 waveform, as well as a weighted averaged x, y, position of the shower 
+///          generated in the one of the 9 blocks taken from the TOPAZ detector. 
+///
+/// \author  lebrun@fnal.gov
+/// \date
+////////////////////////////////////////////////////////////////////////
+#ifndef SIM_TOPAZLGHit_H
+#define SIM_TOPAZLGHit_H
+
+#include <vector>
+
+namespace sim {
+  /// A sum of the contribution due to a given secondary produced from the primary particle 
+  class TOPAZLGHit {
+
+  public:
+    TOPAZLGHit();
+    ~TOPAZLGHit() {}
+    
+    int        GetBlockNumber() const {return fBlock; }
+    double     GetEDeposited() const {return fEDeposited; } // sum of the photons, perhaps, time a calibration fector. Depending on usage.. 
+    double     GetMeanX() const {return fX[0]; } // average over the whole shower, in a given block. 
+    double     GetMeanY() const {return fX[1]; }
+    double     GetMeanZ() const {return fX[2]; }
+    int        GetNumPhot() const {return fNPhots; } // at the PMT cathode, integrated over the waveform 
+    
+    int        GetNumStep() const {return fNSteps; } 
+
+    int        GetAncestorPId() const; // pdg value for the track ID, which is a secondary coming our of the target 
+                                                      // or the primary itself. Pick the one with the highest number visible photons at the PMT cathode.  
+
+    int        GetAncestorTrackNum() const; // Corresponding 
+
+    size_t GetSizeWaveform() const { return fWaveForm.size();} // Assuming a 250 MHz digitization ! 
+    std::vector<int>::const_iterator GetItWaveBegin() const { return fWaveForm.cbegin(); } 
+    std::vector<int>::const_iterator GetItWaveEnd() const { return fWaveForm.cend(); } 
+//    size_t GetSizeTracksAndPid() const { return fTrackAmpls.size();} // Assuming a 250 MHz digitization ! 
+//    std::vector<std::pair<int, int> >::const_iterator GetItTrackAndPidsBegin() const { return fTrackAmpls.cbegin(); } 
+//    std::vector<std::pair<int, int> >::const_iterator GetItTrackAndPidsEnd() const { return fTrackAmpls.cend(); } 
+//
+    void SetBlockNumber(int n) { fBlock = n; }
+ 
+//
+// public method to compute averages for positions and renormalize the waveform, if need be.    
+//
+  void Reset(); // to be invoked at the beginning of each event 
+  void AddAncestorTrack(int ancestorTrackNum); // for each non-vanishing contribution, above 1 MeV, 
+                        // We add the pid and the G4Tack number of the ancestor (actually, the child of the primary track, of the primary track number, 
+			// which is one.
+  void AddSomePhotons(int numPhot, double x, double y, double z, double t); // to be called by the user action upon a new G4Step. 
+                        // !!! modal: add to the last entered element of fTrackAmpls
+    // for each stepif the energy of the chargec particle energy is such that the Cerenkov angle is say, greater than 60 degree, 
+    // we generate number of photons detected at the photocathode  
+  void FinalizeAndCalibrate(double calFact);         
+
+  private:
+  
+    int fBlock; // from 0 to 9
+    double fEDeposited; 
+    int fNSteps;
+    std::vector<double> fX; // dimensioned to 3. Compute averages in FinalizeAndCalibrate,  Irrespectively of the trackNum/PiD that created it.   
+    int fNPhots; // Total, Irrespectively of the G4 track number & PiD
+    int fNunSteps; 
+    
+    std::vector<int> fWaveForm;
+// Too complicated, Root pukes on this.. classes_def.xml difficult to build 
+//    std::vector<std::pair<int, int> > fTrackAmpls;
+//    std::vector<std::pair<int, int> >::reverse_iterator fItTrackLastUsed;
+    //
+
+  };
+
+}
+#endif
+////////////////////////////////////////////////////////////////////////
