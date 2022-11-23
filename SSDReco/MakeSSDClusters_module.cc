cde5d6ab1ec4cb39240c182c7f6a718589254942
Teresa Lackey <lackey32@fnal.gov>
2022-11-23 14:02:14 -0600

    Step through raw digits on each sensor and create separate clusters if there is a gap > RowGap (configurable). Also added optional check for if event had any SSD digits. Still adding to TTree to learn more about cluster behaviour.

--- SSDReco/MakeSSDClusters_module.cc
+++ SSDReco/MakeSSDClusters_module.cc
@@ -21,6 +21,7 @@
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
 #include "ChannelMap/ChannelMapService.h"
+#include "DataQuality/EventQuality.h"
 #include "RawData/SSDRawDigit.h"
 #include "RecoBase/SSDCluster.h"
 
@@ -49,10 +50,17 @@ public:
 
 private:
   
+  art::ServiceHandle<emph::cmap::ChannelMapService> cmap;
   TTree *ssdclust;
   int event;
   std::vector<int> station, sens, ndigits, width, timerange;
-  std::string fSSDRawLabel;
+  std::vector<float> avgadc;
+  
+  // fcl parameters
+  std::string fSSDRawLabel; ///< Data label for SSD Raw Digits
+  bool        fFillTTree;   ///< Fill TTree with plots for debugging/development?
+  int         fRowGap;      ///< Maximum allowed gap between strips for forming clusters
+  bool        fCheckDQ;     ///< Check data quality for event
 
 static  bool CompareByRow(const art::Ptr<emph::rawdata::SSDRawDigit>& a,
 		    const art::Ptr<emph::rawdata::SSDRawDigit>& b);
@@ -66,24 +74,28 @@ static  bool CompareByRow(const art::Ptr<emph::rawdata::SSDRawDigit>& a,
 //--------------------------------------------------
 emph::MakeSSDClusters::MakeSSDClusters(fhicl::ParameterSet const& pset)
   : EDProducer{pset},
-  fSSDRawLabel (pset.get< std::string >("SSDRawLabel"))
+  fSSDRawLabel (pset.get< std::string >("SSDRawLabel")),
+  fFillTTree   (pset.get< bool >("FillTTree")),
+  fRowGap      (pset.get< int >("RowGap")),
+  fCheckDQ     (pset.get< bool >("CheckDQ"))
 {
   this->produces< std::vector<rb::SSDCluster> >();
 }
 
 //--------------------------------------------------
-// dev for alg.
 void emph::MakeSSDClusters::beginJob()
 {
-  art::ServiceHandle<art::TFileService> tfs;
-
-  ssdclust = tfs->make<TTree>("clusts","");
-  ssdclust->Branch("event",&event,"event/I");
-  ssdclust->Branch("station",&station);
-  ssdclust->Branch("sens",&sens);
-  ssdclust->Branch("ndigits",&ndigits);
-  ssdclust->Branch("width",&width);
-  ssdclust->Branch("timerange",&timerange);
+  if (fFillTTree) {
+    art::ServiceHandle<art::TFileService> tfs;
+    ssdclust = tfs->make<TTree>("clusts","");
+    ssdclust->Branch("event",&event,"event/I");
+    ssdclust->Branch("station",&station);
+    ssdclust->Branch("sens",&sens);
+    ssdclust->Branch("ndigits",&ndigits);
+    ssdclust->Branch("width",&width);
+    ssdclust->Branch("timerange",&timerange);
+    ssdclust->Branch("avgadc",&avgadc);
+  }
 }
 
 //--------------------------------------------------
@@ -112,7 +124,7 @@ void emph::MakeSSDClusters::FormClusters(art::PtrVector<emph::rawdata::SSDRawDig
     curRow = dig->Row();
 
     // if gap too big, push cluster and clear it
-    if ( curRow-prevRow > 1) {
+    if ( curRow-prevRow > (fRowGap + 1) ) {
       ssdClust.SetStation(station);
       ssdClust.SetSensor(sensor);
       sensClusters->push_back(ssdClust);
@@ -123,30 +135,38 @@ void emph::MakeSSDClusters::FormClusters(art::PtrVector<emph::rawdata::SSDRawDig
     prevRow=curRow;
   }
 
-  // Dumb clustering - just copy all digits into one cluster for sensor
-  // rb::SSDCluster ssdClust(sensDigits);
-  // ssdClust.SetStation(station);
-  // ssdClust.SetSensor(sensor);
-  // if (ssdClust.NDigits()>0) {
-  //   sensClusters->push_back(ssdClust);
-  // }
-
 }
 
 //--------------------------------------------------
 void emph::MakeSSDClusters::produce(art::Event& evt)
 {
-  event = evt.event();
-  art::ServiceHandle<emph::cmap::ChannelMapService> cmap;
-
   std::unique_ptr< std::vector<rb::SSDCluster> > clusterv(new std::vector<rb::SSDCluster>);
+
+  event = evt.event();
+
+  if (fCheckDQ){
+    art::Handle<dq::EventQuality> eventqual;
+    try{
+      evt.getByLabel("dataqual",eventqual);
+      // if no ssd hits in event, continue
+      if(!eventqual->hasSSDHits){
+	rb::SSDCluster ssdClust;
+	clusterv->push_back(ssdClust);
+	evt.put(std::move(clusterv));
+	return;
+      }
+    }
+    catch(...){
+      std::cout<<"No Data Quality product found in event but CheckDQ set to true!"<<std::endl;
+      abort();
+    }
+  }
+
   art::PtrVector<emph::rawdata::SSDRawDigit> digitList[6][6];
   
   art::Handle< std::vector<emph::rawdata::SSDRawDigit> > ssdHandle;
   try{
     evt.getByLabel(fSSDRawLabel,ssdHandle);
-    // std::vector<art::Ptr<emph::rawdata::SSDRawDigit> > digits;
-    // digits.reserve(ssdHandle->size());
     if (!ssdHandle->empty()) {
       for (size_t idx=0; idx<ssdHandle->size(); ++idx){
 	art::Ptr<emph::rawdata::SSDRawDigit> ssdDig(ssdHandle,idx);
@@ -158,8 +178,6 @@ void emph::MakeSSDClusters::produce(art::Event& evt)
       // Should really pull counts of these from geometry somehow
       for (int sta=0; sta<6; ++sta){
 	for (int sensor=0; sensor<6; ++sensor){
-	  // This needs to be replaced with FormClusters() function that creates multiple clusters per sensor (as needed)
-	  // If we have multiple clusters for a given station,sensor, add ID in
 	  // Don't bother to cluster if we didn't have any raw digits
 	  if (digitList[sta][sensor].size()==0)
 	    continue;
@@ -168,29 +186,17 @@ void emph::MakeSSDClusters::produce(art::Event& evt)
 	  this->SortByRow(digitList[sta][sensor]);
 	  this->FormClusters(digitList[sta][sensor], &clusters, sta, sensor);
 	  for (int i=0; i<(int)clusters.size(); i++){
-	    station.push_back(clusters[i].Station());
-	    sens.push_back(clusters[i].Sensor());
-	    ndigits.push_back(clusters[i].NDigits());
-	    width.push_back(clusters[i].Width());
-	    timerange.push_back(clusters[i].TimeRange());
+	    // fill vectors for optimizing algorithm. This part of module should be removed once it's more finalized.
+	    if (fFillTTree) {
+	      station.push_back(clusters[i].Station());
+	      sens.push_back(clusters[i].Sensor());
+	      ndigits.push_back(clusters[i].NDigits());
+	      width.push_back(clusters[i].Width());
+	      timerange.push_back(clusters[i].TimeRange());
+	      avgadc.push_back(clusters[i].AvgADC());
+	    }
 	    clusterv->push_back(clusters[i]);
-	    //std::cout<<clusters[i]<<std::endl;	    
 	  }
-	  // Trial form cluster here before splitting off into function
-	  
-	  // rb::SSDCluster ssdClust(digitList[sta][sensor]);
-	  // ssdClust.Add(digitList[sta][sensor]);
-	  // ssdClust.SetStation(sta);
-	  // ssdClust.SetSensor(sensor);
-	  //std::cout<<ssdClust<<std::endl;
-	  // if (ssdClust.NDigits()>0) {
-	  //   clusterv->push_back(ssdClust);
-	  //   station.push_back(ssdClust.Station());
-	  //   sens.push_back(ssdClust.Sensor());
-	  //   ndigits.push_back(ssdClust.NDigits());
-	  //   width.push_back(ssdClust.Width());
-	  //   timerange.push_back(ssdClust.TimeRange());
-	  // }
 	}
       }
 	  
@@ -198,16 +204,18 @@ void emph::MakeSSDClusters::produce(art::Event& evt)
   }
   catch(...){
   }
-  ssdclust->Fill();
-  evt.put(std::move(clusterv));
-  //digits.clear();
-  //clusters.clear();
 
-  station.clear();
-  sens.clear();
-  ndigits.clear();
-  width.clear();
-  timerange.clear();
+  evt.put(std::move(clusterv));
+
+  if (fFillTTree) {
+    ssdclust->Fill();
+    station.clear();
+    sens.clear();
+    ndigits.clear();
+    width.clear();
+    timerange.clear();
+    avgadc.clear();
+  }
 }
 
 DEFINE_ART_MODULE(emph::MakeSSDClusters)
