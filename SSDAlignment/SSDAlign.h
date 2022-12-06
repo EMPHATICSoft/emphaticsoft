d816136332db1224621be758c455d0823c91f193
Jack Mirabito <jmirabit@emphaticgpvm03.fnal.gov>
2022-12-06 11:22:18 -0600

    SSDAlignment module added

---
+++ SSDAlignment/SSDAlign.h
@@ -0,0 +1,89 @@
+////////////////////////////////////////////////////////////////////////
+/// \brief   Definition of SSD align class 
+/// \author  jmirabit@bu.edu
+/// \date
+////////////////////////////////////////////////////////////////////////
+#ifndef SSDALIGN_H
+#define SSDALIGN_H
+
+#include <vector>
+#include <stdint.h>
+#include <iostream>
+
+#include "RawData/RawDigit.h"
+#include "RawData/SSDRawDigit.h"
+#include "Geometry/Geometry.h"
+
+namespace emph{
+  namespace al{
+
+
+  class SSDAlign {
+  public:
+    SSDAlign(); // Default constructor
+    SSDAlign(const emph::rawdata::SSDRawDigit &ssdraw, const emph::geo::Detector &sd, const emph::geo::SSDStation &st, int evt); // Default constructor
+    virtual ~SSDAlign() {}; //Destructor
+    
+  private:
+
+    double _angle; // rotation angle about the vertical y-axis
+    double _strip;  // avg. strip position
+    double _pitch;
+    double _height;
+    double _sensor_row;
+    int _fer;
+    int _module;
+	 double _x, _y, _z, _u, _v;
+	 double _xcal, _ycal, _zcal;
+    int _event;
+
+  public:
+    // Getters
+    double Angle() const { return _angle; }
+    double Strip() const { return _strip; }
+    double Pitch() const { return _pitch; }
+    double Height() const { return _height; }
+    double Sensor_Row() const { return _sensor_row; }
+    int FER() const { return _fer; }
+    int Module() const { return _module; }
+    double X() const { return _x; }
+    double Y() const { return _y; }
+    double Z() const { return _z; }
+    double U() const { return _u; }
+    double V() const { return _v; }
+    int Event() const { return _event; }
+
+    // Setters
+    void SetAngle(double ang) { _angle = ang; }
+    void SetStrip(double strip) { _strip = strip; }
+    void SetPitch(double pitch) { _pitch = pitch; }
+    void SetHeight(double height) { _height = height; }
+    void SetSensor_Row(double sensor_row) { _sensor_row = sensor_row; }
+    void SetFER(int fer) { _fer = fer; }
+    void SetModule(int module) { _module = module; }
+    void SetX(double x) { _x = x; }
+    void SetY(double y) { _y = y; }
+    void SetZ(double z) { _z = z; }
+    void SetU(double u) { _u = u; }
+    void SetV(double v) { _v = v; }
+    void SetEvent(int event) { _event = event; }
+
+    
+    std::vector<int> SSDInfo();
+    void SSDHitPosition(std::vector<std::vector<double>>& xpos, std::vector<std::vector<double>>& ypos,std::vector<std::vector<double>>& upos, std::vector<std::vector<double>>& vpos);
+    bool IsSingleHit(std::vector<double>& hits);
+    bool IsAlignmentEvent(std::vector<std::vector<double>> pos);
+    bool IsAlignmentEvent(std::vector<std::vector<double>> xpos,std::vector<std::vector<double>> ypos,std::vector<std::vector<double>>& pos);
+    std::vector<double> PositionAtStations(std::vector<std::vector<double>> pos);
+    std::vector<double> PositionAtStations(std::vector<std::vector<double>> xpos,std::vector<std::vector<double>> ypos, std::vector<std::vector<double>> pos);
+    std::vector<int> IndexAtStations(std::vector<std::vector<double>> pos);
+    std::vector<double> StationZpos();
+    void CalibrateXYZ(double *cal); 
+    
+    friend std::ostream& operator << (std::ostream& o, const SSDAlign& h);
+  };
+
+  }
+}
+
+#endif // SSDALIGN_H
