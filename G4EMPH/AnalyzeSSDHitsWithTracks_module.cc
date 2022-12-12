47b8023d7b87b1380201f8d5caa9d064afbbce7f
Paul L. G. Lebrun <lebrun@fnal.gov>
2022-12-12 08:17:13 -0600

    Add Multiple Scattering Analysis

--- G4EMPH/AnalyzeSSDHitsWithTracks_module.cc
+++ G4EMPH/AnalyzeSSDHitsWithTracks_module.cc
@@ -62,6 +62,8 @@ namespace emph {
 // Some utilities.. 
 //      
       bool fFilesAreOpen;
+      bool fDoPResol1Stu; 
+      bool fDoMult2Stu; 
       std::string fTokenJob;    
       std::string fSSDHitLabel, fTrackLabel;
       unsigned int fRun;
@@ -78,25 +80,28 @@ namespace emph {
       sim::Track *fEmTracks;
       std::vector<double> fZlocXPlanes;
       std::vector<double> fZlocXStations;
+      std::vector<double> fZlocYPlanes;
+      std::vector<double> fZlocYStations;
       std::vector<sim::SSDHit> fSSDVec;
       std::vector<sim::Track> fTrackVec;
 //
 // CSV tuple output..
 // 
-      std::ofstream fFOutA1;
+      std::ofstream fFOutA1, fFOutMult2;
       void openOutputCsvFiles();
       
       void StudyPResol1 (const std::vector<sim::SSDHit> &theSSDHits, const std::vector<sim::Track> &theTracks) ; 
+      void StudyMult2 (const std::vector<sim::SSDHit> &theSSDHits, const std::vector<sim::Track> &theTracks) ; 
       
     }; 
     
 // .....................................................................................
     AnalyzeSSDHitsWithTracks::AnalyzeSSDHitsWithTracks(fhicl::ParameterSet const& pset) : 
     EDAnalyzer(pset), 
-    fFilesAreOpen(false), fTokenJob("undef"), fSSDHitLabel("?"), fTrackLabel("?"),
+    fFilesAreOpen(false), fDoPResol1Stu(false), fDoMult2Stu(false), fTokenJob("undef"), fSSDHitLabel("?"), fTrackLabel("?"),
      fRun(0), fSubRun(0),  fEvtNum(INT_MAX), fNEvents(0) , fPitch(0.06),
      fRunHistory(nullptr), fEmgeo(nullptr), fEmSSDHits(nullptr), fEmTracks(nullptr), 
-     fZlocXPlanes(0), fZlocXStations(0)
+     fZlocXPlanes(0), fZlocXStations(0), fZlocYPlanes(0), fZlocYStations(0) 
     {
        std::cerr << " Constructing AnalyzeSSDHitsWithTracks " << std::endl;
        this->reconfigure(pset);
@@ -106,7 +111,9 @@ namespace emph {
     void AnalyzeSSDHitsWithTracks::reconfigure(const fhicl::ParameterSet& pset)
     {
       fTokenJob = pset.get<std::string>("tokenJob", "UnDef");
-      fSSDHitLabel = pset.get<std::string>("SSDHitLabel");
+      fDoPResol1Stu = pset.get<bool>("doPresol1Stu", false);
+      fDoMult2Stu = pset.get<bool>("doMult2Stu", false);
+     fSSDHitLabel = pset.get<std::string>("SSDHitLabel");
       fTrackLabel = pset.get<std::string>("TrackLabel");
     }
     void AnalyzeSSDHitsWithTracks::beginRun(art::Run const &run)
@@ -119,6 +126,7 @@ namespace emph {
        emph::geo::SSDStation aStation = fEmgeo->GetSSDStation(k);
        TVector3 posSt = aStation.Pos();
        fZlocXStations.push_back(posSt[2]);
+       fZlocYStations.push_back(posSt[2]);
        for (int kk=0; kk != aStation.NSSDs(); kk++) {
          emph::geo::Detector aPlane = aStation.GetSSD(kk);
 	 if ((std::abs(aPlane.Rot() - 270.*M_PI/180.) < 0.1) || (std::abs(aPlane.Rot() - 90.*M_PI/180.) < 0.1) || 
@@ -126,12 +134,22 @@ namespace emph {
 	    TVector3 pos = aPlane.Pos();
 	    fZlocXPlanes.push_back(pos[2] + posSt[2]);
 	 }
+	 if ((std::abs(aPlane.Rot())  < 0.1) || (std::abs(aPlane.Rot() - M_PI/180.) < 0.1) || 
+	       (std::abs(aPlane.Rot() + M_PI/180.) < 0.1) ) {
+	    TVector3 pos = aPlane.Pos();
+	    fZlocYPlanes.push_back(pos[2] + posSt[2]);
+	 }
+	 
        } // on SS Detector planes of segment of a plane.   
      } // on index k, SSD Stations.
      if (fZlocXPlanes.size() < 5)  { std::cerr << " Not enough X planes to do this study, quit here and now " << std::endl; exit(2); }
      std::cerr << " Number of SSD X planes " << fZlocXPlanes.size() << std::endl;
      std::cerr << " Z locations ";
      for (size_t i=0; i != fZlocXPlanes.size(); i++) std::cerr << " " << fZlocXPlanes[i] << ",";
+     std::cerr << " Number of SSD Y planes " << fZlocYPlanes.size() << std::endl;
+     std::cerr << " Z locations ";
+     for (size_t i=0; i != fZlocYPlanes.size(); i++) std::cerr << " " << fZlocYPlanes[i] << ",";
+     
      std::cerr << " Number of SSDStations  " << fZlocXStations.size() << std::endl;
      std::cerr << " Z locations ";
      for (size_t i=0; i != fZlocXStations.size(); i++) std::cerr << " " << fZlocXStations[i] << ",";
@@ -147,12 +165,20 @@ namespace emph {
 	 << " AnalyzeSSDHitsWithTracks::openOutputCsvFiles, run number not yet defined, something faulty in overall flow, quit here and now " << std::endl;
 	 exit(2);
       }
-      std::ostringstream fNamePResol1StrStr; fNamePResol1StrStr << "./PResol1Tuple_V1_" << fRun << "_" << fTokenJob << ".txt";
-      std::string fNamePResol1Str(fNamePResol1StrStr.str());
-      fFOutA1.open(fNamePResol1Str.c_str());
-      fFOutA1 << " subRun evt nTr nTr1G tr x y z pNorm pTrans slx01 slx23 slx45 slx01D slx23D slx45D pMeas pMeasD ";
-      fFOutA1 << " " << std::endl;
-      
+      if (fDoPResol1Stu) { 
+        std::ostringstream fNamePResol1StrStr; fNamePResol1StrStr << "./PResol1Tuple_V1_" << fRun << "_" << fTokenJob << ".txt";
+        std::string fNamePResol1Str(fNamePResol1StrStr.str());
+        fFOutA1.open(fNamePResol1Str.c_str());
+        fFOutA1 << " subRun evt nTr nTr1G tr x y z pNorm pTrans slx01 slx23 slx45 slx01D slx23D slx45D pMeas pMeasD ";
+        fFOutA1 << " " << std::endl;
+      }
+      if (fDoMult2Stu) {
+        std::ostringstream fNameMult2StrStr; fNameMult2StrStr << "./MultStuTuple_V1_" << fRun << "_" << fTokenJob << ".txt";
+        std::string fNameMult2Str(fNameMult2StrStr.str());
+        fFOutMult2.open(fNameMult2Str.c_str());
+        fFOutMult2 << " subRun evt nTr nTrHM kTr y0 y1 y2 y3 y4 y5 "; // simply.. 
+        fFOutMult2 << " " << std::endl;
+      }
       fFilesAreOpen = true;
     }
     void AnalyzeSSDHitsWithTracks::endJob() {
@@ -160,7 +186,8 @@ namespace emph {
       std::cerr << " AnalyzeSSDHitsWithTracks::endJob , for run " << fRun << " last subrun " << fSubRun << std::endl;
       std::cerr << " Number of events " <<  fNEvents << std::endl;
       
-      fFOutA1.close(); 
+      if (fDoPResol1Stu) fFOutA1.close(); 
+      if (fDoMult2Stu) fFOutMult2.close();
     }
     void AnalyzeSSDHitsWithTracks::analyze(const art::Event& evt) {
     //
@@ -187,7 +214,9 @@ namespace emph {
       std::vector<sim::Track> myTracks(*theTracks); // a deep copy that should not be here.. Conveninece for mulyiple analyssis.
 //      std::cerr << " Number of tracks : " << myTracks.size() << std::endl;
       
-     this->StudyPResol1(mySSDHits, myTracks);
+     if (fDoPResol1Stu) this->StudyPResol1(mySSDHits, myTracks);
+     if (fDoMult2Stu) this->StudyMult2(mySSDHits, myTracks);
+     
     } // end of Analyze 
     //
     // simple Xslope measurement 
@@ -242,6 +271,38 @@ namespace emph {
         const double pMeas = ((slx23 != DBL_MAX) && (slx45 != DBL_MAX)) ? 1.0/(slx45 - slx23) : DBL_MAX;
         const double pMeasD = ((slx23D != DBL_MAX) && (slx45D != DBL_MAX)) ? 1.0/(slx45D - slx23D) : DBL_MAX;
 	fFOutA1 << " " << slx01D << " "<< slx23D << " "<< slx45D << " "<< pMeas << " " << pMeasD << std::endl; 
+      } // onTracks.
+   }
+   //
+   // Multiple scattering study, for assign proper uncertainties for alignment.. 
+   // 
+    void AnalyzeSSDHitsWithTracks::StudyMult2 (const std::vector<sim::SSDHit> &theSSDHits, const std::vector<sim::Track> &theTracks) {
+      std::ostringstream prologStrStr;
+      prologStrStr << " " << fSubRun << " " << fEvtNum << " " << theTracks.size();
+      int nTr1G = 0;
+      const std::string prologStr(prologStrStr.str());
+      std::vector<double> yPosHits(6, DBL_MAX);
+      for (std::vector<sim::Track>::const_iterator iTrack = theTracks.cbegin(); iTrack != theTracks.cend(); iTrack++ ) {
+	const double pMom = std::sqrt(iTrack->GetPx()*iTrack->GetPx() +  iTrack->GetPy()*iTrack->GetPy() + iTrack->GetPz()*iTrack->GetPz());
+        if (pMom > 100.) nTr1G++;
+      }
+      if (nTr1G != 1) return; // not interested in hadronic interactions. 
+      for (std::vector<sim::Track>::const_iterator iTrack = theTracks.cbegin(); iTrack != theTracks.cend(); iTrack++ ) {
+	const double pMom = std::sqrt(iTrack->GetPx()*iTrack->GetPx() +  iTrack->GetPy()*iTrack->GetPy() + iTrack->GetPz()*iTrack->GetPz());
+        if (pMom < 100.) continue; // skip the low energy tracks.. We have them tally above. 
+	for (size_t kH = 0; kH != theSSDHits.size(); kH++) {
+	  sim::SSDHit aHit = theSSDHits[kH];
+	  if (aHit.GetTrackID() != iTrack->GetTrackID()) continue;  // first big cheat..
+	  for (size_t kPl = 0; kPl != yPosHits.size(); kPl++) {
+	    if ( std::abs(fZlocYPlanes[kPl] - aHit.GetZ()) < 1.0 )  {
+	       yPosHits[kPl] =  aHit.GetY();
+	     } // got a match in Z  
+	  } 
+	}// Over the hits..
+	fFOutMult2 << prologStr << " " << nTr1G;
+	fFOutMult2 << " " << iTrack->GetTrackID();
+	for (size_t k=0; k != yPosHits.size(); k++) fFOutMult2  << " " << yPosHits[k];
+	fFOutMult2 << std::endl;
       } // onTracks.
    } 
 } // name space emph       
