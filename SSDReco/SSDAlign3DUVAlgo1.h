2a315f4ba4ed0e810b91471493f0e8bb27f7d2b5
Paul L. G. Lebrun <lebrun@fnal.gov>
2022-12-17 06:51:07 -0600

    Starting on the UV crude alignment

---
+++ SSDReco/SSDAlign3DUVAlgo1.h
@@ -0,0 +1,159 @@
+////////////////////////////////////////////////////////////////////////
+/// \brief   2D aligner, X-Z view or Y-Z view, indepedently from each others. 
+///          Algorithm one.  Could beong to SSDCalibration, but, this aligner 
+///          requires some crude track reconstruction, as it is based on track residuals, i
+///          of SSD strip that are on too often. 
+/// \author  lebrun@fnal.gov
+/// \date
+////////////////////////////////////////////////////////////////////////
+#ifndef SSDALIGN3DUVYALGO1_H
+#define SSDALIGN3DUVALGO1_H
+
+#include <vector>
+#include <stdint.h>
+#include <iostream>
+
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Framework/Principal/Event.h"
+#include "canvas/Persistency/Common/Ptr.h"
+#include "canvas/Persistency/Common/PtrVector.h"
+#include "RecoBase/SSDCluster.h"
+#include "SSDReco/SSDAlignSimpleLinFit.h"
+
+namespace emph { 
+  namespace ssdr {
+ 
+    class SSDAlign2DXYAlgo1 {
+    
+       public:
+      
+	SSDAlign2DXYAlgo1(); // No args .. for now.. 
+	SSDAlign2DXYAlgo1(char aView, int aStation); U or V 
+        ~SSDAlign2DXYAlgo1();
+	
+        private:
+	  const size_t fNumStations = 6;
+	  const size_t fNumStrips = 639; // Per wafer. 
+	  const double fOneOverSqrt12; 
+	  int fRunNum;  // The usual Ids for a art::event 
+	  int fSubRunNum;
+	  int fEvtNum;
+	  int fNEvents; // Incremental events count for a given job. 
+	  bool fFilesAreOpen;
+	  char fView; 
+	  int fStation; // the sensor to align..      
+	  double fPitch;
+	  double fHalfWaferWidth;
+	  int fNumIterMax; // Maximum number of iteration 
+	  double fChiSqCut;
+	  std::string fTokenJob;
+	  double fZCoordsMagnetCenter, fMagnetKick120GeV; 
+	  std::vector<double> fZCoords;
+	  std::vector<double> fNominalOffsets; // for station 4 and 5, ?????  
+	  std::vector<double> fNominalOffsetsAlt45; // for station 4 and 5, Y View Sensor 2 
+	  std::vector<double> fMeanResidualsX;// the meanvalue over a run..Or previously fitted..  
+	  std::vector<double> fMeanResidualsY;// the meanvalue over a run..Or previously fitted..  
+// 
+// Additional cuts.. and variables. 
+//	  
+	  std::vector<double> fMinStrips; // to use only to pick the center of the beam.. !! Might be run dependant! 
+	  std::vector<double> fMaxStrips;
+	  std::vector<double> fMultScatUncert;
+	  std::vector<double> fOtherUncert;
+	  std::vector<double> fZLocShifts;// not used 
+	  std::vector<double> fPitchOrYawAngles; // not used 
+//
+	  emph::ssdr::SSDAlignSimpleLinFit myLinFit; 
+
+	  std::ofstream fFOutA1, fFOutA1Dbg;
+	  
+	   
+	public:
+         inline void SetRun(int aRunNum) { fRunNum = aRunNum; } 
+         inline void SetSubRun(int aSubR) { fSubRunNum = aSubR; } 
+	 inline void SetEvtNum(int aEvt) { fEvtNum = aEvt; } 
+	 inline void SetNumIterMax( int n) { fNumIterMax = n; }
+	 inline void SetChiSqCut1 (double v) { fChiSqCut = v; } 
+	 inline void SetTokenJob(const std::string &aT) { fTokenJob = aT; }
+	 inline void SetZLocShifts(const std::vector<double> v) { fZLocShifts = v; } 
+	 inline void SetOtherUncert(const std::vector<double> v) { fOtherUncert = v; } 
+	 inline void SetPitchAngles(const std::vector<double> v) { fPitchOrYawAngles = v; } 
+//	 inline void SetRefPtForPitchOrYawAngle(double v) { fRefPointPitchOrYawAngle = v; }
+	 inline void SetFittedResiduals(std::vector<double> v) { fMeanResiduals = v;} 
+	 inline void SetMagnetKick120GeV(double v) { fMagnetKick120GeV = v; }
+	 void InitializeCoords(bool lastIs4, const std::vector<double> &zCoords);
+	 inline void SetTheView(char aView) {
+	   if ((aView != 'U') && (aView != 'V')) {
+	     std::cerr << " SSDAlign2DXYAlgo1, setting an unknown view " << aView << " fatal, quit here " << std::endl; 
+	     exit(2);
+	   }
+	   fView = aView;
+	 }
+	 inline int RunNum() const { return fRunNum; }
+	 inline int SubRunNum() const { return fSubRunNum; }
+	 
+	 inline double GetTsUncertainty(size_t kSt, std::vector<rb::SSDCluster>::const_iterator itCl) const {
+	  double aRMS = itCl->WgtRmsStrip();
+	  double errMeasSq = fOneOverSqrt12*fPitch * 1.0/(1.0 + aRMS*aRMS); // Very approximate, need a better model. 
+	  return std::sqrt(errMeasSq + fOtherUncert[kSt]*fOtherUncert[kSt] + fMultScatUncert[kSt]*fMultScatUncert[kSt]);
+	  
+	 }
+	 
+	 void  alignIt(const art::Event &evt, const art::Handle<std::vector<rb::SSDCluster> > aSSDClsPtr); 
+	 void  alignItAlt45(const art::Event &evt, 
+	                                 const art::Handle<std::vector<rb::SSDCluster> > fSSDClsPtr ); 
+					 // find the residuals for station 4 & 5, Sensor 4 and 5 ( I think..) 
+	 
+	 private:
+	 
+	 bool recoX(const art::Handle<std::vector<rb::SSDCluster> > aSSDClsPtr) 
+	 
+	 inline double getTsFromCluster(size_t kStation, bool alternate45, double strip) {
+	   switch (fView) { // see SSDCalibration/SSDCalibration_module 
+	     case 'X' :
+	     {
+	       double aVal=0.;
+	       if (kStation < 4) {
+	         aVal =  ( -1.0*strip*fPitch + fNominalOffsets[kStation] + fResiduals[kStation] + fMeanResiduals[kStation]);
+	       } else {
+	         if (!alternate45) aVal =  strip*fPitch - fNominalOffsets[kStation] + fResiduals[kStation] + fMeanResiduals[kStation];
+		 else aVal =  -1.0*strip*fPitch + fNominalOffsetsAlt45[kStation] + fResiduals[kStation] + fMeanResiduals[kStation];
+		 // Momentum correction, for 120 GeV primary beam  
+		 aVal += fMagnetKick120GeV * (fZCoords[kStation] - fZCoordsMagnetCenter);
+		 // Yaw Correction 
+	       }
+	       return aVal;
+	       break;
+	     } 
+	     case 'Y' : 
+	      {
+	       double aVal = 0.;
+	       if (kStation < 4) {  
+	         aVal =  (strip*fPitch + fNominalOffsets[kStation] + fResiduals[kStation] + fMeanResiduals[kStation]);
+	       } else {
+	         if (!alternate45) aVal =  ( -1.0*strip*fPitch + fNominalOffsets[kStation] + fResiduals[kStation] + fMeanResiduals[kStation]);
+		 else aVal =  ( strip*fPitch + fNominalOffsetsAlt45[kStation] + fResiduals[kStation] + fMeanResiduals[kStation]);
+	       } 
+	       const double aValC = this->correctTsForPitchOrYawAngle(kStation, aVal);
+	       return aValC;
+	       break; 
+	      }
+	      default :
+	        std::cerr << " SSDAlign2DXYAlgo1::getTsFromCluster, unexpected view, " 
+		<< fView << " kStation " << kStation << 
+		 " internal error, fatal " << std::endl; exit(2);
+	   }
+	   return 0.; // should not happen  
+	 }
+	 inline double correctTsForPitchOrYawAngle(size_t kStation, double ts) {
+	   return (ts + ts*fPitchOrYawAngles[kStation]);
+	 }
+	 void openOutputCsvFiles();
+	
+    };
+  
+  } // namespace ssdr
+}// namespace emph
+
+#endif // SSDAlign3DUVAlgo1
