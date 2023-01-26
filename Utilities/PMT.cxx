2b93d159206c28d8503bb85b8a34d926f4aead6d
Linyan Wan <linyanwan2009@gmail.com>
2023-01-26 13:21:09 -0600

    Add PMT class in utilities

---
+++ Utilities/PMT.cxx
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////////////////////
+/// \file    PMT.cxx
+/// \brief
+/// \version 
+/// \author  wanly@bu.edu
+////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include "TRandom3.h"
+#include "PMT.h"
+
+namespace emph {
+	namespace arich_util {
+
+		PMT::PMT() :
+			fPMTnum(-1), fDarkRate(0), fQECorrection(1)
+		{
+		}
+
+		//--------------------------------------------------------------------------------
+
+		PMT::PMT(int n, double dark, double qecor, std::vector<std::pair<double, double> > qe) :
+			fPMTnum(n), fDarkRate(dark), fQECorrection(qecor), fQE(qe)
+		{
+		}
+
+		bool PMT::ifDet(double wavelength)
+		{
+			//return true;
+			std::vector<std::pair<double, double> >::const_iterator iter = fQE.begin();
+			std::vector<std::pair<double, double> >::const_iterator iter1 = fQE.end();
+			iter1--;
+			if(wavelength<=iter->first || wavelength>=iter1->first)
+				return false;
+			else
+			{
+				while(wavelength>(iter->first))
+				{
+					iter1 = iter;
+					iter++;
+				}
+				double w1 = iter1->first;
+				double w2 = iter->first;
+				double q1 = iter1->second;
+				double q2 = iter->second;
+				double eff1 = q1+(q2-q1)/(w2-w1)*(wavelength-w1);
+				double eff = eff1*fQECorrection;
+
+				TRandom3 *rand = new TRandom3(0);
+				if( rand->Uniform() <= eff ) return true;
+				else return false;
+			}
+
+			return false;
+		}
+
+
+	} // end namespace geo
+} // end namespace emph
---
+++ Utilities/PMT.h
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////
+/// \file    PMT.h
+/// \brief
+/// \version 
+/// \author  wanly@bu.edu
+////////////////////////////////////////////////////////////////////////
+
+#ifndef ARICH_PMT_H
+#define ARICH_PMT_H
+
+#include <utility>
+#include <vector>
+
+namespace emph {
+	namespace arich_util {
+
+		class PMT {
+			public:
+				PMT();
+				PMT(int n, double dark, double qecor, std::vector<std::pair<double, double> > qe);
+				~PMT() {};
+
+				int PMTnum() const { return fPMTnum;} 
+				double DarkRate() const {return fDarkRate;}
+				double QEcor() const {return fQECorrection;}
+
+				void SetPMTnum(int n) {fPMTnum = n; }
+				void SetDarkRate(double dark) {fDarkRate = dark;}
+				void SetQEcor(double qecor) {fQECorrection = qecor;}
+				void SetQE(std::vector<std::pair<double, double> > qe) {fQE = qe;}
+
+				bool ifDet(double wavelength);
+
+			private:    
+				int fPMTnum;
+				double fDarkRate;
+				double fQECorrection;
+				std::vector<std::pair<double, double> > fQE;
+		};
+
+	}  // end namespace geo
+} // end namespace emph
+
+
+#endif
