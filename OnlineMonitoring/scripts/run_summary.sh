7441895a419f014a2df9ac9c06cb19c9f3de226c
Linyan Wan <linyanwan2009@gmail.com>
2023-03-23 14:58:49 -0500

    Changes to work on the daq machine

--- OnlineMonitoring/scripts/run_summary.sh
+++ OnlineMonitoring/scripts/run_summary.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+# Run this script on a daq machine emphatic-daq-0*
 run=$1
-DIR=/emph/data/data/June2022/OutputData
+DIR=/data2/OutputData/
 TMPFILE=${run}stats.log
-LOGFILE=mylog.log
-n=$(ls $DIR | grep _r${run}_ |wc -l)
-# echo Processing $n spills in run $run
-rm $TMPFILE
-for spill in $(seq $n)
+
+nonew=0
+spill=1
+while [ $nonew -le 5 ]
 do
-  art -c /emph/app/users/twester/build/fcl/rawEventDump_v1.fcl $DIR/emdaq_otsdaq_rootOutput_r${run}_s${spill}_* | grep ID > $TMPFILE 
-  echo Extracting data from spill $run $spill >> $LOGFILE
-  ./analyze_log.sh $run >> $LOGFILE
+spill_format=$(printf "%04d" $spill)
+if test -n "$(find $DIR -maxdepth 1 -name "emdaq_otsdaq_rootOutput_r${run}_s${spill_format}*" -print -quit)"
+then
+  echo running "$DIR/emdaq_otsdaq_rootOutput_r${run}_s${spill_format}*"
+  art -c rawEventDump_v1.fcl $DIR/emdaq_otsdaq_rootOutput_r${run}_s${spill_format}_*  | grep ID | tee $TMPFILE
+  echo Extracting data from spill $run $spill 
+  spill=$(( $spill + 1 ))
+  nonew=0
+else
+  echo No new files yet... 
+  nonew=$(( $nonew +1 ))
+fi
+sleep 3
 done
 
+./analyze_log.sh $run 
