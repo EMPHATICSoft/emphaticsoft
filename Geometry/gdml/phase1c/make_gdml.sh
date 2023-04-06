be28c2400c10451208fed52bf6701d56234a5dc6
Linyan Wan <linyanwan2009@gmail.com>
2023-04-05 19:45:44 -0500

    Add support for phase1c, preliminary

---
+++ Geometry/gdml/phase1c/make_gdml.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#-m: 0: w/o magnet; 1: w/ magnet
+#-t: 0: w/o target; 1: graphite; 2: CH2
+
+$(dirname $0)/generate_gdml.pl -o phase1c.$$.gdml -m 1 -t 1
+$(dirname $0)/../make_gdml.pl -i phase1c.$$.gdml -o phase1c.gdml 
+/bin/rm -f phase1c.$$.gdml phase1_*.gdml
