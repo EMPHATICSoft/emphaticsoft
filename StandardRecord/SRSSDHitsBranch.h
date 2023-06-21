4eb75b006996494c9c01c39b10f94e5d647031a4
Denise Huerta <dhuerta@emphaticgpvm01.fnal.gov>
2023-06-21 13:07:03 -0500

    Standard record files to create an SSD Hits CAF

---
+++ StandardRecord/SRSSDHitsBranch.h
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////
+// \file    SRSSDHitsBranch.h
+////////////////////////////////////////////////////////////////////////
+#ifndef SRSSDHitsBRANCH_H
+#define SRSSDHitsBRANCH_H
+#include "StandardRecord/SRSSDHits.h"
+
+#include <vector>
+
+namespace caf
+{
+  /// Reconstructed rings found by various algorithms
+    class SRSSDHitsBranch
+    {
+    public:
+      SRSSDHitsBranch();// Default constructor
+      ~SRSSDHitsBranch(); //Destructor
+
+      std::vector<SRSSDHits>  hits;
+      size_t                nhits;
+
+      void fillSizes();
+      
+    };
+
+} // end namespace
+
+#endif // SRSSDHitsBRANCH_H
+///////////////////////////////////////////////////////////////////////////
