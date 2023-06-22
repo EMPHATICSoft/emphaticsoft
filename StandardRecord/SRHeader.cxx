608907ad9704badac1c171f55f35b8ee64fd47f2
Jonathan Paley <jpaley@fnal.gov>
2023-06-22 15:57:42 -0500

    Add more spill information, eg, gas ckov pressures, counts, etc.

--- StandardRecord/SRHeader.cxx
+++ StandardRecord/SRHeader.cxx
@@ -13,7 +13,10 @@ namespace caf
   SRHeader::SRHeader():
     run(0),
     subrun(0),
-    evt(0)
+    evt(0),
+    timestamp(0), intensity(0.), momentum(0.), MT5CPR(0.), MT6CPR(0.),
+    MT5CP2(0.), MT6CP2(0.), TBCCDI(0.), TBCCDO(0.), MT6CA1(0.), MT6CA2(0.),
+    MT6CA3(0.)
   {
   }
 
--- StandardRecord/SRHeader.h
+++ StandardRecord/SRHeader.h
@@ -4,6 +4,8 @@
 #ifndef SRHEADER_H
 #define SRHEADER_H
 
+#include <stdint.h>
+
 namespace caf
 {
   /// Header representing overview information for the current event/spill
@@ -16,6 +18,16 @@ namespace caf
     unsigned int   run;       ///< run number
     unsigned int   subrun;    ///< subrun number
     unsigned int   evt;       ///< ART event number
+    uint32_t timestamp;
+    float  intensity;
+    float  momentum;
+    float  MT5CPR, MT6CPR;// gas Ckov pressure readings
+    float  MT5CP2, MT6CP2;// iFix readings
+    float  TBCCDI;         // TestBeam ckov counts inner
+    float  TBCCDO;         // TestBeam ckov counts outer
+    float  MT6CA1;         // MT6 ckov east mirror counts
+    float  MT6CA2;         // MT6 ckov west mirror counts
+    float  MT6CA3;         // MT6 ckov top mirror counts
 
     void setDefault();
   };
