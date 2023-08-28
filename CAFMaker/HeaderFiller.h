ca4a642a07744142502dce7b17c6169385cb1161
Jonathan Paley <jpaley@fnal.gov>
2023-08-28 16:09:14 -0500

    Fix hdr extraction issue, get it from the art::SubRun, not art::Event.

--- CAFMaker/HeaderFiller.h
+++ CAFMaker/HeaderFiller.h
@@ -1,20 +1,23 @@
 ////////////////////////////////////////////////////////////////////////
 // \file    HeaderFiller.h
-// \note    Class to fill CAF Header info
+// \note    Class to fill CAF Header info.  Since the header gets 
+//          information from the subrun object and not the event, this
+//          class does not inherit from the Filler base class. 
 ////////////////////////////////////////////////////////////////////////
 #ifndef HEADERFILLER_H
 #define HEADERFILLER_H
 
-#include "CAFMaker/FillerBase.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "StandardRecord/StandardRecord.h"
 
 namespace caf
 {
   /// Class to fill header information
-  class HeaderFiller : public FillerBase
+  class HeaderFiller 
   {
   public:
 
-    void Fill(art::Event&, caf::StandardRecord&);
+    void Fill(art::SubRun&, caf::SRHeader&);
 
   };
 
