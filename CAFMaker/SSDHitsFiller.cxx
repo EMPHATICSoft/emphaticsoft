4968c3e2dfb930ad88686bdf1482a407fd212ae7
Denise Huerta <dhuerta@emphaticgpvm01.fnal.gov>
2023-09-21 17:15:45 -0500

    changes to include sensor and station in SSDHits

--- CAFMaker/SSDHitsFiller.cxx
+++ CAFMaker/SSDHitsFiller.cxx
@@ -10,6 +10,11 @@
 #include "art/Framework/Principal/Handle.h"
 #include <cxxabi.h>
 
+#include "ChannelMap/service/ChannelMapService.h"
+#include "Geometry/service/GeometryService.h"
+#include "Geometry/Geometry.h"
+#include "ChannelMap/ChannelMap.h"
+
 namespace caf
 {
   void SSDHitsFiller::Fill(art::Event& evt, caf::StandardRecord& stdrec)
@@ -22,13 +27,24 @@ namespace caf
     //   	<< abi::__cxa_demangle(typeid(*hitv).name(), 0, 0, 0)
     //   	<< "' found under label '" << fLabel << "'. " << std::endl;
     // }
+
       
- std::vector <emph::rawdata::SSDRawDigit> ssdhits;
+    std::vector <emph::rawdata::SSDRawDigit> ssdhits;
     if(!hitv.failedToGet()) ssdhits = *hitv;
+
+    auto&& geometryService = art::ServiceHandle<emph::geo::GeometryService>();
+    auto emgeo = geometryService->Geo();
+
+    auto&& channelMapService = art::ServiceHandle<emph::cmap::ChannelMapService>();
+    auto emcmap = channelMapService->CMap();
 
    for (unsigned int hitId = 0; hitId < ssdhits.size(); ++ hitId) {
       stdrec.hits.hits.push_back(SRSSDHits());
       SRSSDHits& srSSDHits = stdrec.hits.hits.back();
+
+      emph::cmap::EChannel echan = emph::cmap::EChannel(emph::cmap::SSD,ssdhits[hitId].FER(),ssdhits[hitId].Module());
+      if (!emcmap->IsValidEChan(echan)) continue;
+      emph::cmap::DChannel dchan = emcmap->DetChan(echan);
 
       srSSDHits.FER = ssdhits[hitId].FER();
       srSSDHits.Module = ssdhits[hitId].Module();
@@ -39,8 +55,8 @@ namespace caf
       srSSDHits.ADC = ssdhits[hitId].ADC();
       srSSDHits.TrigNum = ssdhits[hitId].TrigNum();
       srSSDHits.Row = ssdhits[hitId].Row();
-    } // end for hitId
-   //   std::cout << "loop" << std::endl;
-  }
-
+      srSSDHits.Station = dchan.Station();
+      srSSDHits.Sensor  = dchan.Channel();
+   } // end for hitId
+  }  
 } // end namespace caf
--- StandardRecord/SRSSDHits.cxx
+++ StandardRecord/SRSSDHits.cxx
@@ -17,7 +17,9 @@ namespace caf
     Time(std::numeric_limits<float>::signaling_NaN()),
     ADC(std::numeric_limits<float>::signaling_NaN()),
     TrigNum(std::numeric_limits<float>::signaling_NaN()),
-    Row(std::numeric_limits<float>::signaling_NaN())
+    Station(std::numeric_limits<int>::signaling_NaN()),
+    Sensor(std::numeric_limits<int>::signaling_NaN()),
+    Plane(std::numeric_limits<int>::signaling_NaN())
 
  {
   }
@@ -39,7 +41,9 @@ namespace caf
     ADC = -5;
     TrigNum = -5;
     Row = -5;
+    Station = -5;
+    Sensor = -5;
+    Plane = -5;
   }
 
 } // end namespace
-
