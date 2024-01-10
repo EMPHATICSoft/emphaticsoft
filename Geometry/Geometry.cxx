0a87cfb008d75aacefb1e2bf2f78110b8329e6c3
Teresa Lackey <lackey32@fnal.gov>
2024-01-10 17:40:50 -0600

    Use TMath::Pi() and simplify angle checks to make it more robust and readable.

--- Geometry/Geometry.cxx
+++ Geometry/Geometry.cxx
@@ -78,21 +78,25 @@ namespace emph {
 
     sensorView Detector::View() const
     {
-      float pi  = 3.14159;
+      auto pi = TMath::Pi();
       // Flipped sensors have a Rot angle π/2 out-of-phase so we need
       // to take the flip into account when finding the angle
       float ang = this->Rot() + pi/2*this->IsFlip();
+      // drop down to a value < π for fewer checks in if statements
+      while(ang >= pi)
+	ang-=pi;
+
       // x-view: π/2, 3π/2
-      if (abs(fmod(ang-pi/2,pi)) < 0.2)
+      if ( abs(ang-pi/2) < 0.2)
 	return X_VIEW;
       // y-view: 0,π
-      else if (abs(fmod(ang,pi)) < 0.2)
+      else if (abs(ang) < 0.2)
 	return Y_VIEW;
       // u-view: 3π/4, 7π/4
-      else if (abs(fmod(ang-3*pi/4,pi)) < 0.2)
+      else if (abs(ang-3*pi/4) < 0.2)
 	return U_VIEW;
       // w-view: π/4, 5π/4
-      else if (abs(fmod(ang-pi/4,pi)) < 0.2)
+      else if (abs(ang-pi/4) < 0.2)
 	return W_VIEW;
       return INIT;
     }
