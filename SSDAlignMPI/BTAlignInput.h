48abfe6828760aa32637736f93db8a9982e66c98
Paul L. G. Lebrun <lebrun@fnal.gov>
2024-01-10 08:51:44 -0600

    Adding the Phase1c

--- SSDAlignMPI/BTAlignInput.h
+++ SSDAlignMPI/BTAlignInput.h
@@ -37,7 +37,8 @@ namespace emph{
 	void DumpCVSForR(int aRank, int aSpill, char view, const std::string &token) const ; // just an ASCII version.. one line, one event. 
 	
       private:
-        bool fIsPhase1c; 
+        bool fIsPhase1c;
+	bool fDoRejectMultIn; 
         size_t fKey = 687400; // this must be the word in the binary file. If strictSt6, see below.. 
         size_t fNumStations = 6; // For Phase1b 
 	std::vector<BeamTrackCluster> fDat;
@@ -49,6 +50,7 @@ namespace emph{
 	inline void SetForPhase1c(bool t = true) { fIsPhase1c = t; fNumStations = 7;}
 	inline void SetKey(size_t k) {fKey = k; } 
 	inline bool IsPhase1c() const { return fIsPhase1c; }
+	inline void RejectMultTracks(bool arbOn = true) { fDoRejectMultIn = arbOn; } 
       
         inline size_t GetNumEvts() const {return fDat.size(); } 
 	inline std::vector<BeamTrackCluster>::const_iterator cbegin() const { return fDat.cbegin(); }
@@ -61,7 +63,6 @@ namespace emph{
 	  }
 	  return fDat.cend();
 	}
-       
     
     };
   } // rbal 
