ab94ad3da8c139ca0c6d4e36124aaf89c7c2eb1f
Jack Mirabito <jmirabit@emphaticgpvm02.fnal.gov>
2024-05-03 11:57:38 -0500

    Work in progress: general spacepoint reconstruction from SSDs

--- SpacePointReco/SpacePointReco_module.cc
+++ SpacePointReco/SpacePointReco_module.cc
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////
-/// \brief   Analyzer module to create alignment constants for SSD
+/// \brief   Producer module to create alignment constants for SSD
 ///          alignment
 /// \author  $Author: jmirabit@bu.edu and wanly@bu.edu$
 ////////////////////////////////////////////////////////////////////////
@@ -20,7 +20,7 @@
 #include "TF1.h"
 
 // Framework includes
-#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/EDProducer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
 #include "art/Framework/Principal/Handle.h"
@@ -39,15 +39,15 @@
 #include "Geometry/service/GeometryService.h"
 #include "DetGeoMap/service/DetGeoMapService.h"
 #include "RawData/TRB3RawDigit.h"
-#include "SpacePointReco/SSDAlign.h"
 #include "RecoBase/SSDCluster.h"
+#include "RecoBase/SpacePoint.h"
 
 //using namespace emph;
 
 namespace emph {
 
     ///
-    class SpacePointReco : public art::EDAnalyzer {
+    class SpacePointReco : public art::EDProducer {
         public:
             explicit SpacePointReco(fhicl::ParameterSet const& pset); // Required! explicit tag tells the compiler this is not a copy constructor
             ~SpacePointReco();
@@ -57,7 +57,7 @@ namespace emph {
 
             // Optional if you want to be able to configure from event display, for example
             //void reconfigure(const fhicl::ParameterSet& pset);
-            void SpacePointReco::GetSpacePoint(art::Handle< std::vector<rb::SSDCluster> > &, std::unique_ptr<std::vector<rb::SpacePoint>> & SpacePoints);
+            void GetSpacePoint(art::Handle< std::vector<rb::SSDCluster> > &, std::unique_ptr<std::vector<rb::SpacePoint>> & SpacePoints);
 
             // Optional use if you have histograms, ntuples, etc you want around for every event
             void beginJob();
@@ -71,67 +71,26 @@ namespace emph {
             art::ServiceHandle<emph::cmap::ChannelMapService> cmap;
             art::ServiceHandle<emph::dgmap::DetGeoMapService> dgm;
 
+            int event;
+            std::vector<int> u_stations;
+            std::vector<int> w_stations;
             int nxsensors=0;
             int nysensors=0;
             int nusensors=0;
             int nwsensors=0;
-            std::vector<rb::SSDCluster> clusters;
-            std::vector<emph::al::SSDAlign> ssdvec;
-            std::vector<emph::al::SSDAlign> evt_holder;
-            std::vector<emph::al::SSDAlign> xevt_holder;
-            std::vector<emph::al::SSDAlign> yevt_holder;
-            std::vector<emph::al::SSDAlign> uevt_holder;
-            std::vector<emph::al::SSDAlign> vevt_holder;
             std::vector<TH2F*> fSSD_Profile;
-            std::vector<TH1F*> fXSSD_Profile;
-            std::vector<TH1F*> fYSSD_Profile;
-            std::vector<TH1F*> fUSSD_Profile;
-            std::vector<TH1F*> fWSSD_Profile;
-            std::vector<TH2F*> fadj_SSD_Profile;
-            std::vector<TH1F*> fX_Residual_Init;
-            std::vector<TH1F*> fY_Residual_Init;
-            std::vector<TH1F*> fX_Residual_Fin;
-            std::vector<TH1F*> fY_Residual_Fin;
-            std::vector<TH1F*> fU_Residual_Fin;
-            std::vector<TH1F*> fW_Residual_Fin;
-            TGraph** fxShifts;
-            TGraph** fyShifts;
-            TGraph** fuShifts;
-            TGraph** fwShifts;
-            TGraph* evt_line;
-            TGraph** evt_disp;
-            TGraph** evt_disp_adj;
-            TF1* fit;
             art::ServiceHandle<art::TFileService> tfs;
             int nstations;
             int nssds;
-            int max_sensors=0;
-            double max_z = 0;
             bool first_run=1;
-            std::map<std::vector<int>, int> spsindex;
-            double magnetpos;
-            double x0z=-1; double x1z=-1; double y0z=-1; double y1z=-1;
-            size_t x_fixedstationindex;
-            size_t y_fixedstationindex;
-            std::vector<double> zpos_x;
-            std::vector<double> zpos_y;
-            std::vector<double> zpos_u;
-            std::vector<double> zpos_v;
-            int loops=50;
     };
 
     //.......................................................................
 
     SpacePointReco::SpacePointReco(fhicl::ParameterSet const& pset)
-        : EDAnalyzer(pset)
+        : EDProducer(pset)
     {
         //this->reconfigure(pset);
-        fxShifts = new TGraph*[10];
-        fyShifts = new TGraph*[10];
-        fuShifts = new TGraph*[10];
-        fwShifts = new TGraph*[10];
-        evt_disp = new TGraph*[10];
-        evt_disp_adj = new TGraph*[10];
     }
 
     //......................................................................
@@ -165,66 +124,9 @@ namespace emph {
 
         nstations = emgeo->NSSDStations();
         nssds = emgeo->NSSDs();
-        magnetpos = emgeo->MagnetDSZPos();
 
-        //resizing to fill with z positions
-        std::cout<<"SSD Stations and Axis Indices Below:"<<std::endl;
-        for (int fer=0; fer<10; ++fer){
-            for (int mod=0; mod<6; ++mod){
-                emph::cmap::EChannel echan = emph::cmap::EChannel(emph::cmap::SSD,fer,mod);
-                if (!fChannelMap->IsValidEChan(echan)) continue;
-                emph::cmap::DChannel dchan = fChannelMap->DetChan(echan);
-
-                const emph::geo::SSDStation *st = emgeo->GetSSDStation(dchan.Station());
-                const emph::geo::Plane      *pln = st->GetPlane(dchan.Plane());
-                const emph::geo::Detector   *sd = pln->SSD(dchan.HiLo());
-
-                //rb::SSDCluster null_clust;
-                //emph::al::SSDAlign sensor_info(null_clust,*sd,*st,0);
-                emph::al::SSDAlign sensor_info(*sd,*st);
-                std::vector<int> sps = {dchan.Station(),dchan.Plane(),dchan.HiLo()};
-
-                if(sensor_info.View()==emph::geo::X_VIEW){
-                    spsindex.insert(std::pair<std::vector<int>,int>(sps,nxsensors));
-                    zpos_x.push_back(sensor_info.Z());
-                    if (x0z==-1 && dchan.Station()==0) x0z = sensor_info.Z();
-                    if (x1z==-1 && sensor_info.Z()>magnetpos){
-                        x1z = sensor_info.Z();
-                        x_fixedstationindex = spsindex[sps];
-                    }
-                    nxsensors+=1;
-                }
-                if(sensor_info.View()==emph::geo::Y_VIEW){
-                    spsindex.insert(std::pair<std::vector<int>,int>(sps,nysensors));
-                    zpos_y.push_back(sensor_info.Z());
-                    if (y0z==-1 && dchan.Station()==0) y0z = sensor_info.Z();
-                    if (y1z==-1 && sensor_info.Z()>magnetpos){
-                        y1z = sensor_info.Z();
-                        y_fixedstationindex = spsindex[sps];
-                    }
-                    nysensors+=1;
-                }
-                if(sensor_info.View()==emph::geo::U_VIEW){
-                    spsindex.insert(std::pair<std::vector<int>,int>(sps,nusensors));
-                    zpos_u.push_back(sensor_info.Z());
-                    nusensors+=1;
-                }
-                if(sensor_info.View()==emph::geo::W_VIEW){
-                    spsindex.insert(std::pair<std::vector<int>,int>(sps,nwsensors));
-                    zpos_v.push_back(sensor_info.Z());
-                    nwsensors+=1;
-                }
-                if (sensor_info.Z()>max_z) max_z=sensor_info.Z();
-            }
-        }
         //Remove duplicate members
-        fSSD_Profile.resize(nstations); fadj_SSD_Profile.resize(nstations);
-        fXSSD_Profile.resize(nstations); fYSSD_Profile.resize(nstations);
-        fUSSD_Profile.resize(nstations);
-        fWSSD_Profile.resize(nstations);
-        fX_Residual_Init.resize(nxsensors); fY_Residual_Init.resize(nysensors);
-        fX_Residual_Fin.resize(nxsensors); fY_Residual_Fin.resize(nysensors);
-        fU_Residual_Fin.resize(nxsensors); fW_Residual_Fin.resize(nysensors);
+        fSSD_Profile.resize(nstations);
         std::array<const char*,4> pos_strings = {"x","y","u","v"};
         char hname[64];
         for (int i=0; i<nstations; ++i) {
@@ -249,8 +151,15 @@ namespace emph {
 
     void SpacePointReco::GetSpacePoint(art::Handle< std::vector<rb::SSDCluster> > & clustH, std::unique_ptr<std::vector<rb::SpacePoint>> & SpacePoints)
     {
-        //First we need to initialize SSD configuration and plots based on the geometry
+        //Initialize Plots etc.
         if (first_run) Initialize();
+
+        //Create vectors for containing hits from SSD sensors
+        std::vector<std::vector<std::array<double,2>>> x_hits(nstations);
+        std::vector<std::vector<std::array<double,2>>> y_hits(nstations);
+        std::vector<std::vector<std::array<double,2>>> u_hits(nstations);
+        std::vector<std::vector<std::array<double,2>>> w_hits(nstations);
+
         for (size_t idx=0; idx < clustH->size(); ++idx) {
             const rb::SSDCluster& clust = (*clustH)[idx];
 
@@ -261,12 +170,95 @@ namespace emph {
                 continue;
             }
             dgm->Map()->SSDClusterToLineSegment(clust, ls);
+            //get x,y,z (and convert to u/w if necessary) of line segment
+            double x0[3];
+            double x1[3];
+            double xavg[3];
 
-            int event = evt.event();
+            for (int i=0; i<3; ++i){
+                x0[i] = ls.X0()[i];
+                x1[i] = ls.X1()[i];
+                xavg[i] = (x0[i]+x1[i])/2.;
+            }
+            if (clust.View()==emph::geo::X_VIEW){
+                x_hits[clust.Station()].push_back({xavg[0],xavg[2]});
+            }
+            if (clust.View()==emph::geo::Y_VIEW){
+                y_hits[clust.Station()].push_back({xavg[1],xavg[2]});
+            }
+            if (clust.View()==emph::geo::U_VIEW){
+            std::cout<<"***** UHIT"<<std::endl;
+                double u = (sqrt(2)/2)*(xavg[0]+xavg[1]);
+                u_hits[clust.Station()].push_back({u,xavg[2]});
+            }
+            if (clust.View()==emph::geo::W_VIEW){
+                double w = (sqrt(2)/2)*((-1.)*xavg[0]+xavg[1]);
+                w_hits[clust.Station()].push_back({w,xavg[2]});
+            }
+        }
+        //for (size_t i=0; i<w_hits.size(); ++i){
+        //    std::cout<<"Event: "<<event<<" "<<w_hits[i].size()<<std::endl;
+        //}
 
-            emph::al::SSDAlign hit(clust,event);
-            ssdvec.push_back(hit);
-            ssdvec.back().SetPos(ls);
+        //Loop through all stations
+        for (int i=0; i<nstations; ++i){
+            if(i!=3) continue;
+            //Create Spacepoint if there are no u/w constraints
+            if (u_hits[i].size()==0 && w_hits[i].size()==0){
+                for (size_t j=0; j<x_hits[i].size(); ++j){
+                    for (size_t k=0; k<y_hits[i].size(); ++k){
+                        double x = x_hits[i][j][0];
+                        double y = y_hits[i][k][0];
+                        double z = (1/2.)*(x_hits[i][j][1]+y_hits[i][k][1]);
+                        double pos[3];
+                        pos[0]=x; pos[1]=y; pos[2]=z;
+                        //std::cout<<"Spacepoint (Event "<<event<<") at station "<<i<<": "<<x<<" "<<y<<" "<<z<<std::endl;
+
+                        rb::SpacePoint sp;
+                        sp.SetX(pos);
+                        sp.SetStation(i);
+                        SpacePoints->push_back(sp);
+                    }
+                }
+            }
+            else if (u_hits.size()!=0){
+                for (size_t it=0; it<u_hits.size(); ++it){
+                    std::cout<<"debug 0"<<std::endl;
+                    size_t x_index; size_t y_index;
+                    double final_x=-999; double final_y=-999; double final_z=-999;
+                    std::cout<<"debug 0.5"<<std::endl;
+                    double u = u_hits[i][it][0];
+                    double u_diff = 999;
+                    for (size_t j=0; j<x_hits[i].size(); ++j){
+                    std::cout<<"debug 1"<<std::endl;
+                        for (size_t k=0; k<y_hits[i].size(); ++k){
+                            double x = x_hits[i][j][0];
+                            double y = y_hits[i][k][0];
+                            double u_xy = (sqrt(2)/2)*(x+y);
+
+                            double diff = u_xy-u;
+                            //**** Add extra constraint here to ensure that the wrong x,y points are not being associated with a u hit
+                            if (abs(diff)<u_diff){
+                                u_diff=diff;
+                                final_x = x-diff/sqrt(2.);
+                                final_y = y-diff/sqrt(2.);
+                                final_z = (1/3.)*(x_hits[i][j][1]+y_hits[i][k][1]+u_hits[i][it][1]);
+                                x_index = j;
+                                y_index = k;
+                            }
+                        }
+                    }
+                    double pos[3];
+                    pos[0]=final_x; pos[1]=final_y; pos[2]=final_z;
+                    rb::SpacePoint sp;
+                    sp.SetX(pos);
+                    sp.SetStation(i);
+                    SpacePoints->push_back(sp);
+                    x_hits[i].erase(x_hits[i].begin()+x_index);
+                    y_hits[i].erase(y_hits[i].begin()+y_index);
+                    std::cout<<"Spacepoint (Event "<<event<<") at station "<<i<<": "<<pos[0]<<" "<<pos[1]<<" "<<pos[2]<<std::endl;
+                }
+            }
         }
     }
 
@@ -279,12 +271,12 @@ namespace emph {
         art::Handle< std::vector<rb::SSDCluster> > clustH;
         auto geo = geom->Geo();
         std::unique_ptr<std::vector<rb::SpacePoint> > SpacePointv(new std::vector<rb::SpacePoint>);
+        event = evt.event();
 
         try {
             evt.getByLabel(fClusterLabel, clustH);
             if (!clustH->empty()) {	
-                GetSpacePoint(ADCHandle,  BACkovHitv);
-                
+                GetSpacePoint(clustH,  SpacePointv);
             }
         }
         catch(...) {
