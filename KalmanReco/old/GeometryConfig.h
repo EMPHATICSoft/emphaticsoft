38fae1a65d464b6d236a9b2312ea7f4304b85756
Jonathan Paley <jpaley@fnal.gov>
2025-05-14 11:45:16 -0500

    Remove copy of Matej's code

--- KalmanReco/old/GeometryConfig.h
+++
@@ -1,9 +0,0 @@
-#ifndef GEOMETRYCONFIG_H
-#define GEOMETRYCONFIG_H
-
-#include "Reconstruction.h"
-#include <string>
-
-void ReadGeometryConfig(std::string configFile, Reconstruction &reconstruction);
-
-#endif
--- KalmanReco/old/JSONError.h
+++
@@ -1,8 +0,0 @@
-#ifndef JSONERROR_H
-#define JSONERROR_H
-
-#include "jsmn.h"
-
-void CheckJSMNType(jsmntok_t token, jsmntype_t type, const int line, const std::string func, const std::string file );
-	
-#endif
