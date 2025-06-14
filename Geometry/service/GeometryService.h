0b9ef83a11dd61e08f8042cf3e57f1dd6b4998ed
Gavin Davies <gsdavies@fnal.gov>
2025-06-14 14:32:51 -0500

    fix up warnings about risk of violating the One Definition Rule (ODR):
    ```
    prefer separate compilation units for implementation (IMPL_SOURCE) and
      plugin registration macros (REG_SOURCE, LIBRARIES REG) due to possible
      consequences of One Definition Rule violation
    ```
    As a result, move plugin registration to file of its own, separate from implementation.
    Needed for Geometry and MagneticField

--- Geometry/service/GeometryService.h
+++ Geometry/service/GeometryService.h
@@ -10,10 +10,9 @@
 
 //Framework includes
 #include "fhiclcpp/ParameterSet.h"
-#include "art/Framework/Services/Registry/ServiceMacros.h"
+#include "art/Framework/Services/Registry/ServiceDeclarationMacros.h"
 #include "art/Framework/Services/Registry/ActivityRegistry.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
-#include "art/Framework/Services/Registry/ServiceDeclarationMacros.h"
 #include "art/Framework/Principal/Run.h"
 #include "art/Framework/Principal/SubRun.h"
 
