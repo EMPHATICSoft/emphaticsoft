6e2f3e0fddf5a6ecbce45865de76040838a52a65
Robert Chirco <rchirco@emphaticgpvm03.fnal.gov>
2025-07-30 16:20:38 -0500

    Adding missing file

---
+++ RecoBase/RecoBaseDefs.h
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////
+/// \brief   General class for reconstruction
+/// \author  rchirco@fnal.gov
+/// \date    June 2, 2025 
+////////////////////////////////////////////////////////////////////////
+#ifndef RBDEFS_H
+#define RBDEFS_H
+
+#include <vector>
+#include <stdint.h>
+#include <iostream>
+
+namespace rb {
+ 
+     enum class Region {
+      kRegion1,
+      kRegion2,
+      kRegion3,
+      kRegionNull
+    }; 
+}
+
+#endif // RBDEFS_H
