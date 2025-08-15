2a11221a22637e381c7400f5ff4b07a7d432e270
Gavin Davies <gsdavies@fnal.gov>
2025-08-15 04:47:16 -0500

    Fix naming inconsistencies

--- Modules/Findmillepede_ii.cmake
+++ Modules/Findmillepede_ii.cmake
@@ -5,33 +5,32 @@ Findmillepede_ii
 
 #]============================================]
 
-find_path(millepedeii_INCLUDE_DIRS
+find_path(millepede_ii_INCLUDE_DIRS
   NAMES millepede_ii/Mille.h
   HINTS ENV MILLEPEDE_INC
   REQUIRED
 )
 
-find_library(millepedeii_LIBRARY
+find_library(millepede_ii_LIBRARY
   NAMES mille
   HINTS ENV MILLEPEDE_LIB ENV MILLEPEDE_II_DIR
   PATH_SUFFIXES lib
   REQUIRED
 )
 
-if (millepedeii_INCLUDE_DIRS AND millepedeii_LIBRARY)
-  set(millepedeii_FOUND TRUE)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(millepede_ii
+  REQUIRED_VARS millepede_ii_INCLUDE_DIRS millepede_ii_LIBRARY
+)
+
+if (millepede_ii_FOUND)
   if (NOT TARGET millepede_ii::millepede_ii)
     add_library(millepede_ii::millepede_ii STATIC IMPORTED)
     set_target_properties(millepede_ii::millepede_ii PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${millepedeii_INCLUDE_DIRS}"
-      IMPORTED_LOCATION "${millepedeii_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${millepede_ii_INCLUDE_DIRS}"
+      IMPORTED_LOCATION "${millepede_ii_LIBRARY}"
     )
   endif()
 endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(millepedeii
-  REQUIRED_VARS millepedeii_FOUND millepedeii_INCLUDE_DIRS millepedeii_LIBRARY
-)
-
-mark_as_advanced(millepedeii_INCLUDE_DIRS millepedeii_LIBRARY)
+mark_as_advanced(millepede_ii_INCLUDE_DIRS millepede_ii_LIBRARY)
