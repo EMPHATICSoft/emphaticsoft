be710db936d39cc5631a0139dab23f041ec06a72
Teresa Lackey <lackey32@fnal.gov>
2025-09-02 16:17:16 -0500

    Fixed path to raw SSD files, left in paths for phases 1b and c.

--- SSD_FERMerger/setup.sh
+++ SSD_FERMerger/setup.sh
@@ -1,5 +1,6 @@
 export MERGER_DIR=`pwd`
 
-export MERGER_INPUT_DIR=/emph/data/data/Mar2023/SSD/
+#export MERGER_INPUT_DIR=/exp/emph/data/raw/June2022/SSD/
+export MERGER_INPUT_DIR=/exp/emph/data/raw/Mar2023/SSD/
 export MERGER_OUTPUT_DIR=`pwd`/
 
