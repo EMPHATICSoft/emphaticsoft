54297221b520ee5b0e366dcb720e2a88a562e803
Paul Lebrun <lebrun@fnal.gov>
2026-03-28 11:31:20 -0400

    explicit message when we enable/disable this service

--- Align/service/AlignService.cc
+++ Align/service/AlignService.cc
@@ -20,11 +20,19 @@ namespace emph
   AlignService::AlignService(const fhicl::ParameterSet& pset,
 			     art::ActivityRegistry & reg)
   {
+  
+    std::cerr << " AlignService::AlignService, start.... " << std::endl;
     art::ServiceHandle<runhist::RunHistoryService> rhs;
     art::ServiceHandle<emph::geo::GeometryService> geo;
 
     fAlign = new Align();
-    if (pset.get<bool>("IsDisabled"))
+    bool currentDisableStatus = pset.get<bool>("IsDisabled");
+    if (!currentDisableStatus) {
+       std::cerr << " .... This service should NOT be here when we use the modifed Geometry file. " << std::endl;
+       std::cerr << " .... Needs to review our options.. " << std::endl;
+//       exit(2);
+    } 
+    if (currentDisableStatus)
       fAlign->Disable();
     else
       fAlign->Enable();
@@ -42,6 +50,7 @@ namespace emph
   //----------------------------------------------------------
   void AlignService::preBeginRun(const art::Run& )
   {
+    if (fAlign->IsDisabled()) return;
     art::ServiceHandle<runhist::RunHistoryService> rhs;
 
     fAlign->LoadSSDConsts(rhs->RunHist()->SSDAlignFile());
